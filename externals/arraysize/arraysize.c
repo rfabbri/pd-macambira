b70a18b0361f16dfe655122d1cb0a5670e129fbe
jusu <jusu@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2002-06-23 13:01:23 +0000

    modified header
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@19 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- externals/arraysize/arraysize.c
+++ externals/arraysize/arraysize.c
@@ -1,4 +1,8 @@
-/* This code is too trivial to have a licence or copyright */
+/* This code is too trivial to have a licence or copyright.
+
+   However it was modeled after arraysize from pixlib,
+   http://pix.test.at/pd/pixlib
+*/
 
 /*
 	arraysize -- report the size of an array
