57de9a9642a749288fb660547fa1f803bcecfaa4
fbar <fbar@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2003-04-04 20:32:22 +0000

    Initial import of fluid, external for fluidsynth. This replaces fiiwu/flext-iiwu in the future.
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@537 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- externals/footils/flext-iiwu/fiiwu/main.cpp
+++ externals/footils/flext-iiwu/fiiwu/main.cpp
@@ -2,50 +2,55 @@
 //#
 //#       Filename:  main.cpp
 //#
-//#    Description:  fiiwu~ - external for PD
+//#    Description:  fluid~ - external for PD and Max/MSP
+//#                           useless without the cool fluidsynth from
+//#                           www.fluidsynth.org
 //#
 //#        Version:  1.0
-//#        Created:  08/20/02
+//#        Created:  04/04/03
 //#       Revision:  none
 //#
 //#         Author:  Frank Barknecht  (fbar)
 //#          Email:  fbar@footils.org
-//#      Copyright:  Frank Barknecht , 2002
+//#      Copyright:  Frank Barknecht , 2003
+//#
+//#
+//#        This program is free software; you can redistribute it and/or modify
+//#    it under the terms of the GNU General Public License as published by
+//#    the Free Software Foundation; either version 2 of the License, or
+//#    (at your option) any later version.
+//#
+//#    This program is distributed in the hope that it will be useful,
+//#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//#    GNU General Public License for more details.
+//#
+//#    You should have received a copy of the GNU General Public License
+//#    along with this program; if not, write to the Free Software
+//#    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 //#
 //#=====================================================================================
 
 
 #include <flext.h>
-#include <iiwusynth.h>
+#include <fluidsynth.h>
 
-#if !defined(FLEXT_VERSION) || (FLEXT_VERSION < 303)
-#error You need at least flext version 0.3.3
+// check flext version (actually this runs with flext since 0.3, I guess, but
+// better be sure...
+#if !defined(FLEXT_VERSION) || (FLEXT_VERSION < 400)
+#error You need at least flext version 0.4.0
 #endif
 
 
 // A flext dsp external ("tilde object") inherits from the class flext_dsp 
-class fiiwu: 
+class fluid: 
 	public flext_dsp
 {
-	// Each external that is written in C++ needs to use #defines 
-	// from flbase.h
-	// 
-	// The define
-	// 
-	// FLEXT_HEADER(NEW_CLASS, PARENT_CLASS)
-	// 
-	// should be somewhere in your dsp file.
-	// A good place is here:
-	
-	FLEXT_HEADER(fiiwu, flext_dsp)
+	FLEXT_HEADER(fluid, flext_dsp)
 
 	public:
-		fiiwu(int argc, t_atom *argv) 
+		fluid(int argc, t_atom *argv) 
 		{
-			// The constructor of your class is responsible for
-			// setting up inlets and outlets and for registering
-			// inlet-methods:
-			
 			AddInAnything();         // slurp anything
 			AddOutSignal(2);         // 2 audio out [ == AddOutSignal(2) ]
 			
@@ -57,67 +62,79 @@ class fiiwu:
 			if (sr != 44100.f) 
 			{
 				post("WARNING: Current samplerate %.0f != 44100", sr);
-				post("WARNING: fiiwu~ might be out of tune!");
+				post("WARNING: fluid~ might be out of tune!");
 			}
 			
-			iiwu_synth_settings_t pd_iiwu_settings = IIWU_DEFAULT_SETTINGS;
+			fluid_settings_t * settings = NULL;
+			settings = new_fluid_settings();
 			
-			// plugin synth: AUDIO off
-			pd_iiwu_settings.flags        &= ~IIWU_AUDIO;
-			pd_iiwu_settings.sample_format = IIWU_FLOAT_FORMAT;
+			// fluid_settings_setstr(settings, "audio.driver", "float");
+			
+			// settings:
+			fluid_settings_setnum(settings, "synth.midi-channels", 16);
+			fluid_settings_setnum(settings, "synth.polyphony", 256);
+			fluid_settings_setnum(settings, "synth.gain", 0.600000);
+			fluid_settings_setnum(settings, "synth.sample-rate", 44100.000000);
+			fluid_settings_setstr(settings, "synth.chorus.active", "no");
+			fluid_settings_setstr(settings, "synth.reverb.active", "no");
+			fluid_settings_setstr(settings, "synth.ladspa.active", "no");
+
 			if (sr != 0)
 			{
-				pd_iiwu_settings.sample_rate   = static_cast<int>(sr);
+				fluid_settings_setnum(settings, "synth.sample-rate", sr);
 			}
 			
 	
-			// Create iiwusynth instance:
-			synth = new_iiwu_synth(&pd_iiwu_settings);
+			// Create fluidsynth instance:
+			synth = NULL;
+			synth = new_fluid_synth(settings);
 			
 			if ( synth == NULL )
 			{
-			        post("fiiwu~: couldn't create synth\n");
+			        post("fluid~: couldn't create synth\n");
 			}
 			
 			// try to load argument as soundfont
-			iiwu_load(argc, argv);
+			fluid_load(argc, argv);
 
 
-			FLEXT_ADDMETHOD_(0,"load", iiwu_load);
-			FLEXT_ADDMETHOD_(0,"note", iiwu_note);
-			FLEXT_ADDMETHOD_(0,"prog", iiwu_program_change);
-			FLEXT_ADDMETHOD_(0,"control", iiwu_control_change);
-			FLEXT_ADDMETHOD_(0,"bend", iiwu_pitch_bend);
-			FLEXT_ADDMETHOD_(0,"bank",  iiwu_bank);
+			FLEXT_ADDMETHOD_(0,"load", fluid_load);
+			FLEXT_ADDMETHOD_(0,"note", fluid_note);
+			FLEXT_ADDMETHOD_(0,"prog", fluid_program_change);
+			FLEXT_ADDMETHOD_(0,"control", fluid_control_change);
+			FLEXT_ADDMETHOD_(0,"bend", fluid_pitch_bend);
+			FLEXT_ADDMETHOD_(0,"bank",  fluid_bank);
 			
-			// list input calls iiwu_note(...)
-			FLEXT_ADDMETHOD_(0, "list",  iiwu_note);
+			// list input calls fluid_note(...)
+			FLEXT_ADDMETHOD_(0, "list",  fluid_note);
 			
 			// some alias shortcuts:
-			FLEXT_ADDMETHOD_(0,"n",  iiwu_note);
-			FLEXT_ADDMETHOD_(0,"p",  iiwu_program_change);
-			FLEXT_ADDMETHOD_(0,"c",  iiwu_control_change);
-			FLEXT_ADDMETHOD_(0,"cc", iiwu_control_change);
-			FLEXT_ADDMETHOD_(0,"b",  iiwu_pitch_bend);
+			FLEXT_ADDMETHOD_(0,"n",  fluid_note);
+			FLEXT_ADDMETHOD_(0,"p",  fluid_program_change);
+			FLEXT_ADDMETHOD_(0,"c",  fluid_control_change);
+			FLEXT_ADDMETHOD_(0,"cc", fluid_control_change);
+			FLEXT_ADDMETHOD_(0,"b",  fluid_pitch_bend);
 			
+			
+			if (settings != NULL )
+				delete_fluid_settings(settings);
 			
 			// We're done constructing:
-			post("-- fiiwu~ with flext ---");
+			if (synth)
+				post("-- fluid~ with flext ---");
 	
 		} // end of constructor
-		~fiiwu()
+		~fluid()
 		{
 			if ( synth != NULL )
-			{
-			        delete_iiwu_synth(synth);
-			}
+			        delete_fluid_synth(synth);
 		}
 	
 	
 		virtual void m_help()
 		{
 			const char * helptext = 
-			"_ __fiiwu~_ _  a soundfont external for Pd and Max/MSP \n"
+			"_ __fluid~_ _  a soundfont external for Pd and Max/MSP \n"
 			"_ argument: \"/path/to/soundfont.sf\" to load on object creation\n"
 			"_ messages: \n"
 			"load /path/to/soundfont.sf2  --- Loads a Soundfont \n"
@@ -125,6 +142,12 @@ class fiiwu:
 			"                                 channel-# note-#  veloc-#\n"
 			"n 0 0 0                      --- Play note, same as above\n"
 			"0 0 0                        --- Play note, same as above\n"
+			"control 0 0 0                --- set controller\n"
+			"c       0 0 0                --- set controller, shortcut\n"
+			"prog 0 0                     --- progam change, \n"
+			"                                 args: channel-# prog-#\n"
+			"p    0 0                     --- program change, shortcut\n"
+
 			;
 			post("%s", helptext);
 
@@ -134,52 +157,56 @@ class fiiwu:
 		virtual void m_signal(int n, float *const *in, float *const *out);
 		
 	private:	
-		iiwu_synth_t *synth;
+		fluid_synth_t *synth;
 		
-		FLEXT_CALLBACK_G(iiwu_load)
-		void iiwu_load(int argc, t_atom *argv);
+		FLEXT_CALLBACK_G(fluid_load)
+		void fluid_load(int argc, t_atom *argv);
 		
-		FLEXT_CALLBACK_G(iiwu_note)
-		void iiwu_note(int argc, t_atom *argv);
+		FLEXT_CALLBACK_G(fluid_note)
+		void fluid_note(int argc, t_atom *argv);
 		
-		FLEXT_CALLBACK_G(iiwu_program_change)
-		void iiwu_program_change(int argc, t_atom *argv);
+		FLEXT_CALLBACK_G(fluid_program_change)
+		void fluid_program_change(int argc, t_atom *argv);
 		
-		FLEXT_CALLBACK_G(iiwu_control_change)
-		void iiwu_control_change(int argc, t_atom *argv);
+		FLEXT_CALLBACK_G(fluid_control_change)
+		void fluid_control_change(int argc, t_atom *argv);
 		
-		FLEXT_CALLBACK_G(iiwu_pitch_bend)
-		void iiwu_pitch_bend(int argc, t_atom *argv);
+		FLEXT_CALLBACK_G(fluid_pitch_bend)
+		void fluid_pitch_bend(int argc, t_atom *argv);
 		
-		FLEXT_CALLBACK_G(iiwu_bank)
-		void iiwu_bank(int argc, t_atom *argv);
+		FLEXT_CALLBACK_G(fluid_bank)
+		void fluid_bank(int argc, t_atom *argv);
 
-}; // end of class declaration for fiiwu
+}; // end of class declaration for fluid
 
 
-// Before we can run our fiiwu-class in PD, the object has to be registered as a
+// Before we can run our fluid-class in PD, the object has to be registered as a
 // PD object. Otherwise it would be a simple C++-class, and what good would
 // that be for?  Registering is made easy with the FLEXT_NEW_* macros defined
 // in flext.h. 
 
-FLEXT_NEW_TILDE_G("fiiwu~", fiiwu)
+FLEXT_NEW_TILDE_G("fluid~", fluid)
 
 
-void fiiwu::iiwu_load(int argc, t_atom *argv)
+void fluid::fluid_load(int argc, t_atom *argv)
 {
-	if (synth == NULL) return;
+	if (synth == NULL) 
+	{
+		post("No fluidsynth, duh");
+		return;
+	}
 	
 	if (argc >= 1 && IsSymbol(argv[0]))	
 	{
 		const char* filename = GetString(argv[0]);
-		if ( iiwu_synth_sfload(synth, filename) == 0)
+		if ( fluid_synth_sfload(synth, filename, 0) == 0)
 		{
 			post("Loaded Soundfont: %s", filename);
 		}
 	}
 }
 
-void fiiwu::iiwu_note(int argc, t_atom *argv)
+void fluid::fluid_note(int argc, t_atom *argv)
 {
 	if (synth == NULL) return;
 	if (argc == 3)
@@ -188,11 +215,11 @@ void fiiwu::iiwu_note(int argc, t_atom *argv)
 		chan  = GetAInt(argv[0]);
 		key   = GetAInt(argv[1]);
 		vel   = GetAInt(argv[2]);
-		iiwu_synth_noteon(synth,chan-1,key,vel);
+		fluid_synth_noteon(synth,chan-1,key,vel);
 	}
 }
 
-void fiiwu::iiwu_program_change(int argc, t_atom *argv)
+void fluid::fluid_program_change(int argc, t_atom *argv)
 {	
 	if (synth == NULL) return;
 	if (argc == 2)
@@ -200,11 +227,11 @@ void fiiwu::iiwu_program_change(int argc, t_atom *argv)
 		int   chan, prog;
 		chan  = GetAInt(argv[0]);
 		prog  = GetAInt(argv[1]);
-		iiwu_synth_program_change(synth,chan-1,prog);
+		fluid_synth_program_change(synth,chan-1,prog);
 	}
 }
 
-void fiiwu::iiwu_control_change(int argc, t_atom *argv)
+void fluid::fluid_control_change(int argc, t_atom *argv)
 {
 	if (synth == NULL) return;
 	if (argc == 3)
@@ -213,11 +240,11 @@ void fiiwu::iiwu_control_change(int argc, t_atom *argv)
 		chan  = GetAInt(argv[0]);
 		ctrl  = GetAInt(argv[1]);
 		val   = GetAInt(argv[2]);
-		iiwu_synth_cc(synth,chan-1,ctrl,val);
+		fluid_synth_cc(synth,chan-1,ctrl,val);
 	}
 }
 
-void fiiwu::iiwu_pitch_bend(int argc, t_atom *argv)
+void fluid::fluid_pitch_bend(int argc, t_atom *argv)
 {
 	if (synth == NULL) return;
 	if (argc == 2)
@@ -225,11 +252,11 @@ void fiiwu::iiwu_pitch_bend(int argc, t_atom *argv)
 		int   chan, val;
 		chan  = GetAInt(argv[0]);
 		val   = GetAInt(argv[1]);
-		iiwu_synth_pitch_bend(synth, chan-1, val);
+		fluid_synth_pitch_bend(synth, chan-1, val);
 	}
 }
 
-void fiiwu::iiwu_bank(int argc, t_atom *argv)
+void fluid::fluid_bank(int argc, t_atom *argv)
 {
 	if (synth == NULL) return;
 	if (argc == 2)
@@ -237,7 +264,7 @@ void fiiwu::iiwu_bank(int argc, t_atom *argv)
 		int   chan, bank;
 		chan  = GetAInt(argv[0]);
 		bank  = GetAInt(argv[1]);
-		iiwu_synth_bank_select(synth, chan-1, bank);
+		fluid_synth_bank_select(synth, chan-1, bank);
 	}
 }
 
@@ -250,12 +277,14 @@ void fiiwu::iiwu_bank(int argc, t_atom *argv)
 //          These are arrays of the signals in the objects signal inlets rsp.
 //          oulets. We come to that later inside the function.
 
-void fiiwu::m_signal(int n, float *const *in, float *const *out)
+void fluid::m_signal(int n, float *const *in, float *const *out)
 {
+	
+	if (synth == NULL) return;
 	
 	float *left  = out[0];
 	float *right = out[1];
 	
-	iiwu_synth_write_float(synth, n, left, 0, 1, right, 0, 1); 
+	fluid_synth_write_float(synth, n, left, 0, 1, right, 0, 1); 
 	
 }  // end m_signal
