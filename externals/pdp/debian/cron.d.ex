bc3685d75b7a835a9cff181438b124aa5036b115
caedesv <caedesv@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2003-09-07 21:08:04 +0000

    Adding debian dir for 'pd-pdp' packet creation
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@938 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

---
+++ externals/pdp/debian/cron.d.ex
@@ -0,0 +1,4 @@
+#
+# Regular cron jobs for the pdp package
+#
+0 4	* * *	root	pdp_maintenance
---
+++ externals/pdp/debian/menu.ex
@@ -0,0 +1,2 @@
+?package(pdp):needs=X11|text|vc|wm section=Apps/see-menu-manual\
+  title="pdp" command="/usr/bin/pdp"
