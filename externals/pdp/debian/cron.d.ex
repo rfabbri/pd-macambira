6d0580b3d539a27fb4947e18cc938096f14a90e7
ggeiger <ggeiger@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2003-11-03 19:03:37 +0000

    removed example files
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@1156 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- externals/pdp/debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the pdp package
-#
-0 4	* * *	root	pdp_maintenance
--- externals/pdp/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(pdp):needs=X11|text|vc|wm section=Apps/see-menu-manual\
-  title="pdp" command="/usr/bin/pdp"
