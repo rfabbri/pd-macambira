af21b94438095e8966cb5adf5a92a32641a68f2f
bbogart <bbogart@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2004-03-20 23:52:48 +0000

    Made minor documentation changes to popup. Added entry external.
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@1459 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- externals/bbogart/entry/README.txt
+++ externals/bbogart/entry/README.txt
@@ -1,18 +1,18 @@
-This is the readme for "popup" a popup menu for PD. 
+This is the readme for "entry" a text entry widget for PD. 
 
-popup is Copyright Ben Bogart 2003
+entry is Copyright Ben Bogart 2003
 
 If you have any questions/comments you can reach the author at ben@ekran.org.
 
 This program is distributed under the terms of the GNU General Public 
 License 
 
-popup is free software; you can redistribute it and/or modify
+entry is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2 of the License, or 
 (at your option) any later version.
 
-popup is distributed in the hope that it will be useful, 
+entry is distributed in the hope that it will be useful, 
 but WITHOUT ANY WARRANTY; without even the implied warranty of 
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details. 
@@ -26,13 +26,14 @@ USAGE:
 Put the binary in your extra folder.
 Put the helpfile in your 5.reference folder.
 
-Arguments: [pixel width] [background colour] [name] [opt1] [opt2] [...]
+Arguments: [pixel width] [pixel height] [background colour] [foreground colour]
 
 Methods:
 
-  float			Select index value
-  name [name]		Popup's name
+  bang			Output text
   bgcolour [colour]	Background Colour (white, green, #5500ff)
-  options [opt1] [...]	List of the popup options
+  fgcolour [colour]	Foreground Colour (white, green, #5500ff)
+  clear			Clear text
+  set [list]		Set text
 
 Have Fun.
--- externals/bbogart/entry/entry.c
+++ externals/bbogart/entry/entry.c
@@ -1,21 +1,19 @@
-/* Popup menu widget for PD                                              *
+/* text entry widget for PD                                              *
  * Based on button from GGEE by Guenter Geiger                           *
  * Copyright Ben Bogart 2004 ben@ekran.org                               * 
 
  * This program is distributed under the terms of the GNU General Public *
  * License                                                               *
 
- * popup is free software; you can redistribute it and/or modify           *
+ * entry is free software; you can redistribute it and/or modify         *
  * it under the terms of the GNU General Public License as published by  *
  * the Free Software Foundation; either version 2 of the License, or     *
  * (at your option) any later version.                                   *
 
- * popup is distributed in the hope that it will be useful,                *
+ * entry is distributed in the hope that it will be useful,              *
  * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
  * GNU General Public License for more details.                          */
-
-
 
 #include <m_pd.h>
 #include <g_canvas.h>
@@ -37,14 +35,11 @@
 #define IOWIDTH 4
 #endif
 
-#define MAX_OPTIONS 100
-
-typedef struct _popup
+typedef struct _entry
 {
      t_object x_obj;
 
      t_glist * x_glist;
-	 t_outlet* out2;
      int x_rect_width;
      int x_rect_height;
      t_symbol*  x_sym;
@@ -52,20 +47,18 @@ typedef struct _popup
      int x_height;
      int x_width;
 	 
-	 int x_num_options;	 
-	 t_symbol* x_colour;
-     t_symbol* x_name;
-	
-	 t_symbol* x_options[MAX_OPTIONS];
+     t_symbol* x_bgcolour;
+     t_symbol* x_fgcolour;
+     t_symbol* x_contents;
 
-} t_popup;
+} t_entry;
 
 /* widget helper functions */
 
 #define DEBUG(x)
 
 
-static void draw_inlets(t_popup *x, t_glist *glist, int firsttime, int nin, int nout)
+static void draw_inlets(t_entry *x, t_glist *glist, int firsttime, int nin, int nout)
 {
  /* outlets */
      int n = nin;
@@ -110,7 +103,7 @@ static void draw_inlets(t_popup *x, t_glist *glist, int firsttime, int nin, int 
 }
 
 
-static void draw_handle(t_popup *x, t_glist *glist, int firsttime) {
+static void draw_handle(t_entry *x, t_glist *glist, int firsttime) {
   int onset = text_xpix(&x->x_obj, glist) + (x->x_rect_width - IOWIDTH+2);
 
   if (firsttime)
@@ -126,28 +119,24 @@ static void draw_handle(t_popup *x, t_glist *glist, int firsttime) {
 	     onset + IOWIDTH-2, text_ypix(&x->x_obj, glist) + x->x_rect_height-4);
 }
 
-static void create_widget(t_popup *x, t_glist *glist)
+static void create_widget(t_entry *x, t_glist *glist)
 {
   char text[MAXPDSTRING];
   int len,i;
   t_canvas *canvas=glist_getcanvas(glist);
+  /* I guess this is for fine-tuning of the rect size based on width and height? */
   x->x_rect_width = x->x_width;
   x->x_rect_height =  x->x_height+2;
   
-  /* Create menubutton and empty menu widget -- maybe the menu should be created elseware?*/
-  sys_vgui("set %xw .x%x.c.s%x ; menubutton $%xw -relief raised -background \"%s\" -text \"%s\" -direction flush -menu $%xw.menu ; menu $%xw.menu -tearoff 0\n",
-		x,canvas,x,x,x->x_colour->s_name,x->x_name->s_name,x,x);
-
-  for(i=0 ; i<x->x_num_options ; i++)
-  {
-	sys_vgui(".x%x.c.s%x.menu add command -label \"%s\" -command {.x%x.c.s%x configure -text \"%s\" ; popup_sel%x \"%d\"} \n", 
-		canvas, x, x->x_options[i]->s_name, canvas, x, x->x_options[i]->s_name, x, i);
-  }
+  /* Create text widget */
+  sys_vgui("set %xw .x%x.c.s%x ; text $%xw -font {helvetica 10} -border 1 -highlightthickness 1 -relief sunken -bg \"%s\" -fg \"%s\" \n",
+		x,canvas,x,x,x->x_bgcolour->s_name,x->x_fgcolour->s_name);
+  sys_vgui("bind .x%x.c.s%x <Leave> {focus [winfo parent .x%x.c.s%x]} \n", canvas, x, canvas, x);
+  sys_vgui("namespace eval entry%x {} \n", x);
   
-  DEBUG(post("id: .x%x.c.s%x", canvas, x);)
 }
 
-static void popup_drawme(t_popup *x, t_glist *glist, int firsttime)
+static void entry_drawme(t_entry *x, t_glist *glist, int firsttime)
 {
   t_canvas *canvas=glist_getcanvas(glist);
   DEBUG(post("drawme %d",firsttime);)
@@ -155,23 +144,23 @@ static void popup_drawme(t_popup *x, t_glist *glist, int firsttime)
        DEBUG(post("glist %x canvas %x",x->x_glist,canvas);)
        create_widget(x,glist);	       
        x->x_glist = canvas;
-       sys_vgui(".x%x.c create window %d %d -width %d -height 25 -anchor nw -window .x%x.c.s%x -tags %xS\n", 
-		canvas,text_xpix(&x->x_obj, glist), text_ypix(&x->x_obj, glist), x->x_width, x->x_glist,x,x);
+       sys_vgui(".x%x.c create window %d %d -anchor nw -window .x%x.c.s%x -tags %xS -width %d -height %d \n", 
+		canvas,text_xpix(&x->x_obj, glist), text_ypix(&x->x_obj, glist),x->x_glist,x,x, x->x_width, x->x_height);
               
      }     
      else {
        sys_vgui(".x%x.c coords %xS \
-%d %d\n",
+                %d %d\n",
 		canvas, x,
 		text_xpix(&x->x_obj, glist), text_ypix(&x->x_obj, glist));
      }
-     draw_inlets(x, glist, firsttime, 2,1);
+     draw_inlets(x, glist, firsttime, 1,1);
      //     draw_handle(x, glist, firsttime);
 
 }
 
 
-static void popup_erase(t_popup* x,t_glist* glist)
+static void entry_erase(t_entry* x,t_glist* glist)
 {
      int n;
 
@@ -189,14 +178,14 @@ static void popup_erase(t_popup* x,t_glist* glist)
 	
 
 
-/* ------------------------ popup widgetbehaviour----------------------------- */
+/* ------------------------ text widgetbehaviour----------------------------- */
 
 
-static void popup_getrect(t_gobj *z, t_glist *owner,
+static void entry_getrect(t_gobj *z, t_glist *owner,
     int *xp1, int *yp1, int *xp2, int *yp2)
 {
     int width, height;
-    t_popup* s = (t_popup*)z;
+    t_entry* s = (t_entry*)z;
 
     width = s->x_rect_width;
     height = s->x_rect_height;
@@ -206,10 +195,10 @@ static void popup_getrect(t_gobj *z, t_glist *owner,
     *yp2 = text_ypix(&s->x_obj, owner) + height;
 }
 
-static void popup_displace(t_gobj *z, t_glist *glist,
+static void entry_displace(t_gobj *z, t_glist *glist,
     int dx, int dy)
 {
-    t_popup *x = (t_popup *)z;
+    t_entry *x = (t_entry *)z;
     DEBUG(post("displace");)
     x->x_obj.te_xpix += dx;
     x->x_obj.te_ypix += dy;
@@ -220,15 +209,15 @@ static void popup_displace(t_gobj *z, t_glist *glist,
 	       text_xpix(&x->x_obj, glist), text_ypix(&x->x_obj, glist)-1,
 	       text_xpix(&x->x_obj, glist) + x->x_rect_width, text_ypix(&x->x_obj, glist) + x->x_rect_height-2);
       
-      popup_drawme(x, glist, 0);
+      entry_drawme(x, glist, 0);
       canvas_fixlinesfor(glist_getcanvas(glist),(t_text*) x);
     }
     DEBUG(post("displace end");)
 }
 
-static void popup_select(t_gobj *z, t_glist *glist, int state)
+static void entry_select(t_gobj *z, t_glist *glist, int state)
 {
-     t_popup *x = (t_popup *)z;
+     t_entry *x = (t_entry *)z;
      if (state) {
 	  sys_vgui(".x%x.c create rectangle \
 %d %d %d %d -tags %xSEL -outline blue\n",
@@ -243,7 +232,7 @@ static void popup_select(t_gobj *z, t_glist *glist, int state)
      }
 }
 
-static void popup_activate(t_gobj *z, t_glist *glist, int state)
+static void entry_activate(t_gobj *z, t_glist *glist, int state)
 {
 /* What does this do, why commented out? 
     t_text *x = (t_text *)z;
@@ -251,16 +240,16 @@ static void popup_activate(t_gobj *z, t_glist *glist, int state)
     if (z->g_pd != gatom_class) rtext_activate(y, state);*/
 }
 
-static void popup_delete(t_gobj *z, t_glist *glist)
+static void entry_delete(t_gobj *z, t_glist *glist)
 {
     t_text *x = (t_text *)z;
     canvas_deletelinesfor(glist_getcanvas(glist), x);
 }
 
        
-static void popup_vis(t_gobj *z, t_glist *glist, int vis)
+static void entry_vis(t_gobj *z, t_glist *glist, int vis)
 {
-    t_popup* s = (t_popup*)z;
+    t_entry* s = (t_entry*)z;
     t_rtext *y;
     DEBUG(post("vis: %d",vis);)
     if (vis) {
@@ -269,26 +258,26 @@ static void popup_vis(t_gobj *z, t_glist *glist, int vis)
 #else
         y = (t_rtext *) rtext_new(glist, (t_text *)z,0,0);
 #endif
-	 popup_drawme(s, glist, 1);
+	 entry_drawme(s, glist, 1);
     }
     else {
 	y = glist_findrtext(glist, (t_text *)z);
-	 popup_erase(s,glist);
+	 entry_erase(s,glist);
 	rtext_free(y);
     }
 }
 
-static void popup_save(t_gobj *z, t_binbuf *b);
+static void entry_save(t_gobj *z, t_binbuf *b);
 
-t_widgetbehavior   popup_widgetbehavior = {
-  w_getrectfn:  popup_getrect,
-  w_displacefn: popup_displace,
-  w_selectfn:   popup_select,
-  w_activatefn: popup_activate,
-  w_deletefn:   popup_delete,
-  w_visfn:      popup_vis,
+t_widgetbehavior   entry_widgetbehavior = {
+  w_getrectfn:  entry_getrect,
+  w_displacefn: entry_displace,
+  w_selectfn:   entry_select,
+  w_activatefn: entry_activate,
+  w_deletefn:   entry_delete,
+  w_visfn:      entry_vis,
 #if PD_MINOR_VERSION < 37
-  w_savefn:     popup_save,
+  w_savefn:     entry_save,
 #endif
   w_clickfn:    NULL,
 #if PD_MINOR_VERSION < 37
@@ -296,156 +285,149 @@ t_widgetbehavior   popup_widgetbehavior = {
 #endif
 }; 
 
-static void popup_output(t_popup* x, t_floatarg popup_index)
+/* Function to reset the contents of the entry box */
+static void entry_set(t_entry* x,  t_symbol *s, int argc, t_atom *argv)
 {
-  outlet_symbol(x->out2, x->x_options[(int)popup_index]);
-  outlet_float(x->x_obj.ob_outlet, popup_index); 
+  int i;
+  t_symbol *tmp;
+  
+  sys_vgui(".x%x.c.s%x delete 0.0 end \n", x->x_glist, x);
+  for(i=0; i<argc ; i++)
+  {
+	tmp = atom_getsymbol(argv+i);
+	sys_vgui("lappend ::entry%x::list %s \n", x, tmp->s_name );
+  }
+  sys_vgui(".x%x.c.s%x insert end $::entry%x::list ; unset ::entry%x::list \n", x->x_glist, x, x, x );
+}
+
+/* Clear the contents of the text widget */
+static void entry_clear(t_entry* x)
+{
+  sys_vgui(".x%x.c.s%x delete 0.0 end \n", x->x_glist, x);
+}
+
+/* Output the symbol */
+/* , t_symbol *s, int argc, t_atom *argv) */
+static void entry_output(t_entry* x, t_symbol *s, int argc, t_atom *argv)
+{
+  outlet_list(x->x_obj.ob_outlet, s, argc, argv );
+}
+
+/* Pass the contents of the text widget onto the entry_output fuction above */
+static void entry_bang_output(t_entry* x)
+{
+  sys_vgui("pd [concat entry%p output [.x%x.c.s%x get 0.0 end] \\;]\n", x, x->x_glist, x);
+  sys_vgui("bind .x%x.c.s%x <Leave> {focus [winfo parent .x%x.c.s%x]} \n", x->x_glist, x, x->x_glist, x);
 }
 
 
-static void popup_save(t_gobj *z, t_binbuf *b)
+static void entry_save(t_gobj *z, t_binbuf *b)
 {
 	int i;
-    t_popup *x = (t_popup *)z;
+    t_entry *x = (t_entry *)z;
 
-    binbuf_addv(b, "ssiisiss", gensym("#X"),gensym("obj"),
+    binbuf_addv(b, "ssiisiiss", gensym("#X"),gensym("obj"),
 		x->x_obj.te_xpix, x->x_obj.te_ypix ,  
-		gensym("popup"), x->x_width, x->x_colour, x->x_name);
-	/* Loop for menu items */
-	for(i=0 ; i<x->x_num_options ; i++)
-	{
-		binbuf_addv(b, "s", x->x_options[i]);
-	}
+		gensym("entry"), x->x_width, x->x_height, x->x_bgcolour, x->x_fgcolour);
     binbuf_addv(b, ";");
 }
 
-/* function to change the popup's menu */
-void popup_options(t_popup* x, t_symbol *s, int argc, t_atom *argv)
+/* function to change colour of text background */
+void entry_bgcolour(t_entry* x, t_symbol* bgcol)
 {
-	int i;
-
-	x->x_num_options = argc;
-	
-	/* delete old menu items */
-	sys_vgui(".x%x.c.s%x.menu delete 0 end \n", x->x_glist, x);
-
-	for(i=0 ; i<argc ; i++)
-	{
-		x->x_options[i] = atom_getsymbol(argv+i);
-		sys_vgui(".x%x.c.s%x.menu add command -label \"%s\" -command {.x%x.c.s%x configure -text \"%s\" ; popup_sel%x \"%d\"} \n", 
-			x->x_glist, x, x->x_options[i]->s_name, x->x_glist, x, x->x_options[i]->s_name, x, i);
-	}
+	x->x_bgcolour->s_name = bgcol->s_name;
+	sys_vgui(".x%x.c.s%x configure -background \"%s\" \n", x->x_glist, x, bgcol->s_name);
 }
 
-/* function to change colour of popup background */
-void popup_bgcolour(t_popup* x, t_symbol* col)
+/* function to change colour of text foreground */
+void entry_fgcolour(t_entry* x, t_symbol* fgcol)
 {
-	x->x_colour->s_name = col->s_name;
-	sys_vgui(".x%x.c.s%x configure -background \"%s\"\n", x->x_glist, x, col->s_name);
+	x->x_fgcolour->s_name = fgcol->s_name;
+	sys_vgui(".x%x.c.s%x configure -foreground \"%s\" \n", x->x_glist, x, fgcol->s_name);
 }
 
-/* Function to change name of popup */
-static void popup_name(t_popup* x, t_symbol *name)
+static t_class *entry_class;
+
+
+static void *entry_new(t_symbol *s, int argc, t_atom *argv)
 {
-	x->x_name->s_name = name->s_name;
-	sys_vgui(".x%x.c.s%x configure -text \"%s\"\n", x->x_glist, x, name->s_name);
-}
-
-/* Function to select a menu option by inlet */
-static void popup_iselect(t_popup* x, t_floatarg item)
-{
-	int i=(int)item;
-	if( i<x->x_num_options && i>=0)
-	{
-		sys_vgui(".x%x.c.s%x configure -text \"%s\" ; popup_sel%x \"%d\" \n",
-			glist_getcanvas(x->x_glist), x, x->x_options[i]->s_name,x, i);
-	} else {
-		post("popup: Valid menu selections are from %d to %d\npopup: You entered %d.", 0, x->x_num_options, i);
-	}
-}
-
-static t_class *popup_class;
-
-
-static void *popup_new(t_symbol *s, int argc, t_atom *argv)
-{
-    t_popup *x = (t_popup *)pd_new(popup_class);
+    t_entry *x = (t_entry *)pd_new(entry_class);
     int i;
-	char buf[256];
+    char buf[256];
 
-    x->x_glist = (t_glist*)NULL;
+    /*x->x_glist = (t_glist*)NULL;*/
+    /*x->x_glist = canvas_getcurrent();*/
 
     
-    x->x_height = 25;
+    x->x_height = 1;
 	
 	if (argc < 4)
 	{
-		post("popup: You must enter at least 4 arguments. Default values used.");
+		post("entry: You must enter at least 4 arguments. Default values used.");
 		x->x_width = 124;
-		x->x_num_options = 1; 
-		x->x_colour = gensym("#ffffff");
-		x->x_name = gensym("popup");
-		
-		x->x_options[0] = gensym("option");
+		x->x_height = 100;
+		x->x_bgcolour = gensym("grey70");
+		x->x_fgcolour = gensym("black");
 		
 	} else {
 		/* Copy args into structure */
 		x->x_width = atom_getint(argv);
-		x->x_colour = atom_getsymbol(argv+1);
-		x->x_name = atom_getsymbol(argv+2);
-		
-		x->x_num_options = argc-3;
-		
-		for(i=0 ; i<x->x_num_options ; i++)
-		{
-			x->x_options[i] = atom_getsymbol( argv+(i+3) );
-		}
+		x->x_height = atom_getint(argv+1);
+		x->x_bgcolour = atom_getsymbol(argv+2);
+		x->x_fgcolour = atom_getsymbol(argv+3);
 	}	
 
-	/* Bind the recieve "popup%p" to the widget outlet*/
-    sprintf(buf,"popup%p",x);
+
+     /*x->x_width = x->x_char_width*5;  
+     x->x_height = (int)x->x_char_height*0.7;
+    x->x_width = x->x_char_width*7;     tuned for Linux 
+     x->x_height = x->x_char_height*5; */
+
+    /* Bind the recieve "entry%p" to the widget instance*/
+    sprintf(buf,"entry%p",x);
     x->x_sym = gensym(buf);
     pd_bind(&x->x_obj.ob_pd, x->x_sym);
-
-	/* define proc in tcl/tk where "popup%p" is the receive, "output" is the method, and "$index" is an argument. */
-    sys_vgui("proc popup_sel%x {index} {\n pd [concat popup%p output $index \\;]\n }\n",x,x); 
-
+	
     outlet_new(&x->x_obj, &s_float);
-	x->out2 = outlet_new(&x->x_obj, &s_symbol);
     return (x);
 }
 
-void popup_setup(void) {
-    popup_class = class_new(gensym("popup"), (t_newmethod)popup_new, 0,
-				sizeof(t_popup),0,A_GIMME,0);
+void entry_setup(void) {
+    entry_class = class_new(gensym("entry"), (t_newmethod)entry_new, 0,
+				sizeof(t_entry),0,A_GIMME,0);
 				
-	class_addmethod(popup_class, (t_method)popup_output,
-								  gensym("output"),
-								  A_DEFFLOAT,
-								  0);
-
-	class_addmethod(popup_class, (t_method)popup_name,
-								  gensym("name"),
-								  A_DEFSYMBOL,
-								  0);
+	class_addbang(entry_class, (t_method)entry_bang_output);
+	
+	class_addmethod(entry_class, (t_method)entry_output,
+        gensym("output"),
+        A_GIMME,
+        0);
 								  
-	class_addmethod(popup_class, (t_method)popup_options,
-								  gensym("options"),
-								  A_GIMME,
-								  0);
+	class_addmethod(entry_class, (t_method)entry_set,
+        gensym("set"),
+        A_GIMME,
+        0);
 								  
-	class_addmethod(popup_class, (t_method)popup_bgcolour,
-								  gensym("bgcolour"),
-								  A_DEFSYMBOL,
-								  0);
-	class_doaddfloat(popup_class, (t_method)popup_iselect);
-
-    class_setwidget(popup_class,&popup_widgetbehavior);
+	class_addmethod(entry_class, (t_method)entry_clear,
+        gensym("clear"),
+        0);
+								  
+	class_addmethod(entry_class, (t_method)entry_bgcolour,
+        gensym("bgcolour"),
+        A_DEFSYMBOL,
+        0);
+								  
+	class_addmethod(entry_class, (t_method)entry_fgcolour,
+        gensym("fgcolour"),
+        A_DEFSYMBOL,
+        0);
+								  
+    class_setwidget(entry_class,&entry_widgetbehavior);
 #if PD_MINOR_VERSION >= 37
-    class_setsavefn(popup_class,&popup_save);
+    class_setsavefn(entry_class,&entry_save);
 #endif
 
-	post("Popup v0.1 Ben Bogart.");
+	post("Text v0.1 Ben Bogart.");
 }
 
 
