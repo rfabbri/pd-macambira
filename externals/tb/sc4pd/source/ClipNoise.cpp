0efd1d3dee4328895544312647e0ac0b22837418
timblech <timblech@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2004-07-14 16:21:44 +0000

    This commit was generated by cvs2svn to compensate for changes in r1857, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@1858 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

---
+++ externals/tb/sc4pd/source/ClipNoise.cpp
@@ -0,0 +1,130 @@
+/* sc4pd 
+   ClipNoise, ClipNoise~
+
+   Copyright (c) 2004 Tim Blechmann.
+
+   This code is derived from:
+	SuperCollider real time audio synthesis system
+    Copyright (c) 2002 James McCartney. All rights reserved.
+	http://www.audiosynth.com
+
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+   Based on:
+     PureData by Miller Puckette and others.
+         http://www.crca.ucsd.edu/~msp/software.html
+     FLEXT by Thomas Grill
+         http://www.parasitaere-kapazitaeten.net/ext
+     SuperCollider by James McCartney
+         http://www.audiosynth.com
+     
+   Coded while listening to: Henry Threadgill: Everybodys Mouth's A Book
+   
+*/
+
+#include <flext.h>
+#include "SC_PlugIn.h"
+#include "support.hpp"
+
+
+#if !defined(FLEXT_VERSION) || (FLEXT_VERSION < 406)
+#error You need at least FLEXT version 0.4.6
+#endif
+
+
+/* ------------------------ ClipNoise~ -------------------------------*/
+
+class ClipNoise_ar:
+    public flext_dsp
+{
+    FLEXT_HEADER(ClipNoise_ar,flext_dsp);
+    
+public:
+    ClipNoise_ar(int argc, t_atom *argv);
+    
+protected:
+    virtual void m_signal(int n, t_sample *const *in, t_sample *const *out);
+    
+private:
+    RGen rgen;
+};
+
+FLEXT_LIB_DSP_V("ClipNoise~",ClipNoise_ar);
+
+ClipNoise_ar::ClipNoise_ar(int argc, t_atom *argv)
+{
+
+    //parse arguments
+    AtomList Args(argc,argv);
+
+    rgen.init(0); //set seed to 0
+
+    AddOutSignal();
+}    
+
+
+void ClipNoise_ar::m_signal(int n, t_sample *const *in, 
+			       t_sample *const *out)
+{
+    t_sample *nout = *out;
+
+    RGET;
+
+    for (int i = 0; i!= n;++i)
+    {
+	(*(nout)++) = fcoin(s1, s2, s3);
+    }
+    
+    RPUT;
+}
+
+
+/* ------------------------ ClipNoise ---------------------------------*/
+
+class ClipNoise_kr:
+    public flext_base
+{
+    FLEXT_HEADER(ClipNoise_kr,flext_base);
+
+public:
+    ClipNoise_kr(int argc, t_atom *argv);
+    
+protected:
+    void m_perform();
+    
+private:
+    RGen rgen;
+    FLEXT_CALLBACK(m_perform);
+};
+
+FLEXT_LIB_V("ClipNoise",ClipNoise_kr);
+
+ClipNoise_kr::ClipNoise_kr(int argc, t_atom *argv)
+{
+    FLEXT_ADDBANG(0,m_perform);
+
+    //parse arguments
+    AtomList Args(argc,argv);
+    
+    rgen.init(0); //set seed to 0
+
+    AddOutFloat();
+}
+
+void ClipNoise_kr::m_perform()
+{
+    ToOutFloat(0,rgen.fcoin());
+}
---
+++ externals/tb/sc4pd/source/main.cpp
@@ -0,0 +1,91 @@
+/* sc4pd    library initialization
+
+   Copyright (c) 2004 Tim Blechmann.               
+
+   This code is derived from:
+	SuperCollider real time audio synthesis system
+    Copyright (c) 2002 James McCartney. All rights reserved.
+	http://www.audiosynth.com
+
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,             
+   but WITHOUT ANY WARRANTY; without even the implied warranty of         
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+   Based on:
+     PureData by Miller Puckette and others.
+         http://www.crca.ucsd.edu/~msp/software.html
+     FLEXT by Thomas Grill
+         http://www.parasitaere-kapazitaeten.net/ext
+     SuperCollider by James McCartney
+         http://www.audiosynth.com
+     
+   Coded while listening to: Phosphor
+
+*/
+
+#include <flext.h>
+
+#if !defined(FLEXT_VERSION) || (FLEXT_VERSION < 406)
+#error You need at least FLEXT version 0.4.6
+#endif
+
+#define SC4PD_VERSION "0.01"
+
+void sc4pd_library_setup()
+{
+    post("\nsc4pd: by tim blechmann");
+    post("based on SuperCollider by James McCartney");
+    post("version "SC4PD_VERSION);
+    post("compiled on "__DATE__);
+    post("contains: Dust(~), MantissaMask(~), Hasher(~), Median(~), "
+	 "BrownNoise(~),");
+    post("          ClipNoise(~), GrayNoise(~), Dust2(~), WhiteNoise(~), "
+	 "PinkNoise(~)\n");
+
+    //initialize objects
+    FLEXT_DSP_SETUP(Dust_ar);
+    FLEXT_SETUP(Dust_kr);
+
+    FLEXT_DSP_SETUP(MantissaMask_ar);
+    FLEXT_SETUP(MantissaMask_kr);
+
+    FLEXT_DSP_SETUP(Hasher_ar);
+    FLEXT_SETUP(Hasher_kr);
+    
+    FLEXT_DSP_SETUP(Median_ar);
+    FLEXT_SETUP(Median_kr);
+
+    FLEXT_DSP_SETUP(BrownNoise_ar);
+    FLEXT_SETUP(BrownNoise_kr);
+
+    FLEXT_DSP_SETUP(ClipNoise_ar);
+    FLEXT_SETUP(ClipNoise_kr);
+
+    FLEXT_DSP_SETUP(GrayNoise_ar);
+    FLEXT_SETUP(GrayNoise_kr);
+
+    FLEXT_DSP_SETUP(WhiteNoise_ar);
+    FLEXT_SETUP(WhiteNoise_kr);
+
+    FLEXT_DSP_SETUP(PinkNoise_ar);
+    FLEXT_SETUP(PinkNoise_kr);
+
+    FLEXT_DSP_SETUP(Dust2_ar);
+    FLEXT_SETUP(Dust2_kr);
+
+    FLEXT_DSP_SETUP(Crackle_ar);
+    FLEXT_SETUP(Crackle_kr);
+}
+
+FLEXT_LIB_SETUP(sc4pd,sc4pd_library_setup);
