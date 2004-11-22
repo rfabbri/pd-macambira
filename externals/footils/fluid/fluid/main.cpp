caec37b430bab28d8746923e083f8494ac0e5403
fbar <fbar@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2004-11-22 13:26:30 +0000

    small change in dsp method
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@2297 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- externals/footils/fluid/fluid/main.cpp
+++ externals/footils/fluid/fluid/main.cpp
@@ -314,8 +314,9 @@ void fluid::m_signal(int n, float *const *in, float *const *out)
 	
 	float *left  = out[0];
 	float *right = out[1];
-	left[0] = 1;
-	right[0] = 1;
+    // Did I put this in???
+    //	left[0] = 1;
+    //	right[0] = 1;
 	fluid_synth_write_float(synth, n, left, 0, 1, right, 0, 1); 
 	
 }  // end m_signal
