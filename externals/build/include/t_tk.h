6089cf8b45557f4e45f9277d5e9156ee6cb8c675
eighthave <eighthave@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2005-04-12 15:59:04 +0000

    - added headers from 0.38-4 to externals/build/include and added README so   people don't repeat the same mistake of removing/etc.
    - added Pd src to include search before externals/build/include so if the Pd
      src is in the standard dev layout, externals will be compiled with those
      headers instead of externals/build/include
    
    
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@2744 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- externals/build/include/t_tk.h
+++ externals/build/include/t_tk.h
@@ -1,1 +1,10 @@
-#include "../../pd/src/t_tk.h"
+/* Copyright (c) 1997-1999 Miller Puckette.
+* For information on usage and redistribution, and for a DISCLAIMER OF ALL
+* WARRANTIES, see the file, "LICENSE.txt," in this distribution.  */
+
+void pdgui_vmess(char *fmt, ...);
+void pdgui_mess(char *s);
+
+void pdgui_evalfile(char *s);
+
+#define GUISTRING 1000
