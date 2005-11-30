07b4ec63e9885f621be529c55193a7dd601fa520
eighthave <eighthave@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2005-11-30 06:01:43 +0000

    added Gem and packages
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@4084 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- scripts/checkout-developer-layout.sh
+++ scripts/checkout-developer-layout.sh
@@ -12,32 +12,40 @@
 
 function print_usage ()
 {
-	 echo " "
-	 echo "Usage: $0 [sourceforge ID]"
-	 echo "   if no ID is given, it will check out anonymously"
-	 echo " "
-	 exit
+    echo " "
+    echo "Usage: $0 [sourceforge ID]"
+    echo "   if no ID is given, it will check out anonymously"
+    echo " "
+    exit
 }
 
 if [ $# -eq 0 ]; then
-	 CVSROOT=":pserver:anonymous@cvs.sourceforge.net:/cvsroot/pure-data"
-	 echo "Checking out anonymously. Give your SourceForge ID as an argument otherwise."
-	 echo "The anonymous password is: anoncvs"
-	 cvs login
+    CVSROOT=":pserver:anonymous@cvs.sourceforge.net:/cvsroot/pure-data"
+    echo "Checking out anonymously. Give your SourceForge ID as an argument otherwise."
+    echo "The anonymous password is: anoncvs"
+    cvs login
 elif [ "$1" == "--help" ]; then
-	 print_usage
+    print_usage
 elif [ "$1" == "-h" ]; then
-	 print_usage
+    print_usage
 elif [ $# -eq 1 ]; then
-	 CVSROOT=":ext:${1}@cvs.sourceforge.net:/cvsroot/pure-data"
+    CVSROOT=":ext:${1}@cvs.sourceforge.net:/cvsroot/pure-data"
 else
-	 print_usage
+    print_usage
 fi
 
-mkdir pure-data && cd pure-data
-for section in abstractions doc extensions externals pd scripts; do
-	 cvs checkout $section
+mkdir pure-data
+cd pure-data
+
+for section in abstractions doc extensions externals pd packages scripts; do
+    cvs checkout $section
 done
+
+# Gem is still separate
+CVSROOT := :pserver:anonymous@cvs.gem.iem.at:/cvsroot/pd-gem
+cvs login
+cvs checkout Gem GemLibs
+
 
 # make the symlinks which simulate the files being installed into the packages
 cd packages && make devsymlinks
