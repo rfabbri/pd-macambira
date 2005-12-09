fa2fb8d095261ef57ce9d00706eade39112d127d
timblech <timblech@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2005-12-09 12:50:06 +0000

    improved find_hid searching capabilities
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@4179 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- abstractions/tb/find_hid.py
+++ abstractions/tb/find_hid.py
@@ -16,28 +16,60 @@
 # the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 # Boston, MA 02111-1307, USA.
 #
-# $Id: find_hid.py,v 1.1 2005-12-04 18:11:08 timblech Exp $
+# $Id: find_hid.py,v 1.2 2005-12-09 12:50:06 timblech Exp $
 #
 
 from os import popen, listdir
+
+def parse_device(event, rules):
+	pipe = popen('udevinfo -a -p /sys/class/input/%s' % event)
+	
+	line = pipe.readline()
+	while line:
+		line.strip()
+		if '==' in line:
+			setting, value = line.split('==')
+			setting = setting.strip()
+			if '{' in setting:
+				setting = setting.split('{')[1].split('}')[0]
+			value = value.strip().strip('"')
+
+			if setting in rules:
+				if rules [setting] == value:
+					# we matched one rule, so we remove it from the pending rules
+					del rules[setting]
+					
+		line = pipe.readline()
+
+		if len(rules) == 0:
+			pipe.close()
+			print event
+			return float(event.strip("event"))
+
+	pipe.close()
+	return -1
 
 def find (*args):
 	name = ""
 	for token in args:
 		name+=" " + str(token)
+
 	name = name.strip()
+	rules = name.split('" "')
+	rules = map (lambda x: x.strip('"'), rules)
+
+	ruledict = dict()
+
+	for rule in rules:
+		setting, value = rule.split('=',1)
+		ruledict[setting] = value
+	rules = ruledict
 
 	events = filter(lambda x: "event" in x,  listdir('/sys/class/input/'))
-	
+
 	for event in events:
-		pipe = popen('udevinfo -a -p /sys/class/input/%s' % event)
-
-		line = pipe.readline()
-		while line:
-			if name in line:
-				print event
-				return float(event.strip("event"))
-
-			line = pipe.readline()
-		pipe.close()
+		ret = parse_device(event, dict(rules))
+		if ret != -1:
+			return int(ret)
+		
 	return -1
