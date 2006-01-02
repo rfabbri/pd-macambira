9e4069a108816ad53774b214c40c14a05b25814e
matju <matju@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2006-01-02 07:20:49 +0000

    new external [rtimer]
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@4352 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- abstractions/pureunity/pureunity.c
+++ abstractions/pureunity/pureunity.c
@@ -1,9 +1,40 @@
+#include <sys/time.h>
 /*#include <m_pd.h>*/
 #include "../../pd/src/m_pd.h"
-void pureunity_setup() {
-  t_pd *m = &pd_objectmaker;
-  class_addcreator((t_newmethod)getfn(m,gensym( "inlet" )),gensym("f.inlet" ),A_GIMME,0);
-  class_addcreator((t_newmethod)getfn(m,gensym( "inlet~")),gensym("~.inlet" ),A_GIMME,0);
-  class_addcreator((t_newmethod)getfn(m,gensym("outlet" )),gensym("f.outlet"),A_GIMME,0);
-  class_addcreator((t_newmethod)getfn(m,gensym("outlet~")),gensym("~.outlet"),A_GIMME,0);
+#define ALIAS(y,x) class_addcreator((t_newmethod)getfn(m,gensym(x)),gensym(y),A_GIMME,0);
+
+typedef struct {
+	t_text o;
+	struct timeval t0;
+} t_rtimer;
+
+t_class *rtimer_class;
+void rtimer_reset(t_rtimer *self) {gettimeofday(&self->t0,0);}
+void *rtimer_new(t_symbol *s) {
+	t_rtimer *self = (t_rtimer *)pd_new(rtimer_class);
+	inlet_new((t_text *)self, (t_pd *)self, gensym("bang"), gensym("1_bang"));
+	outlet_new((t_text *)self, gensym("float"));
+	rtimer_reset(self);
+	return self;
 }
+
+void rtimer_1_bang(t_rtimer *self) {
+	struct timeval t1;
+	gettimeofday(&t1,0);
+	outlet_float(self->o.ob_outlet,
+		(t1.tv_sec -self->t0.tv_sec )*1000.0 +
+		(t1.tv_usec-self->t0.tv_usec)/1000.0);
+}
+
+void pureunity_setup() {
+	t_pd *m = &pd_objectmaker;
+	ALIAS( "f.inlet","inlet"  );
+	ALIAS( "~.inlet","inlet~" );
+	ALIAS("f.outlet","outlet" );
+	ALIAS("~.outlet","outlet~");
+	ALIAS(  "f.swap","swap"   );
+	rtimer_class = class_new(gensym("rtimer"),(t_newmethod)rtimer_new,0,sizeof(t_rtimer),0,0);
+	class_addbang(rtimer_class,rtimer_reset);
+	class_addmethod(rtimer_class, (t_method)rtimer_1_bang, gensym("1_bang"), 0);
+}
+
