f80dc7521d6fbb328a592dae283d596d7b549f9b
matju <matju@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2006-01-06 20:19:28 +0000

    .
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@4365 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- abstractions/pureunity/pureunity.c
+++ abstractions/pureunity/pureunity.c
@@ -1,3 +1,25 @@
+/*
+	$Id: pureunity.c,v 1.4 2006-01-06 20:19:28 matju Exp $
+	PureUnity
+	Copyright 2006 by Mathieu Bouchard <matju à artengine point ca>
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	See file ./COPYING for further informations on licensing terms.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
+
 #include <sys/time.h>
 /*#include <m_pd.h>*/
 #include "../../pd/src/m_pd.h"
