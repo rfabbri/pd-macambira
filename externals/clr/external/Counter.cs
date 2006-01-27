b5665533a89356981fd00b1a2eb8d4dbaae22840
xovo <xovo@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2006-01-27 22:51:15 +0000

    Counter external class with new layout, usign virtual functions
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@4507 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- externals/clr/external/Counter.cs
+++ externals/clr/external/Counter.cs
@@ -1,52 +1,74 @@
 using System;
 
-namespace PureData
+/// <summary>
+/// Descrizione di riepilogo per Counter.
+/// </summary>
+public class Counter:
+	PureData.External
 {
-	/// <summary>
-	/// Descrizione di riepilogo per Counter.
-	/// </summary>
-	public class Counter
+	public Counter()
 	{
-		private IntPtr x;
-		
-		int curr = 0;
+		Post("Count");
 
-		public Counter()
-		{
-			curr = 0;
-		}
+//        EventFloat += new MethodFloat(MyFloat);
+    }
 
-		public Counter(float f)
-		{
-			curr = (int) f;
-		}
+	public Counter(PureData.Atom[] args)
+	{
+        Post("Count with args");
 
-		// this function MUST exist
-		public void SetUp()
-		{
-			pd.AddSelector(x, "init", new pd.DelegateFloat(Init));
-			pd.AddSelector(x, new pd.DelegateWithoutArguments(SendOut));
-			pd.AddSelector(x, new pd.DelegateFloat(Sum));
-			pd.AddInlet(x, "init", ParametersType.Float);
-			pd.AddOutlet(x, ParametersType.Float);
+//        pd.AddInlet(x, "init", ParametersType.Float);
+//        pd.AddOutlet(x, ParametersType.Float);
+    }
 
-		}
-
-		public void Init(float f)
-		{
-			curr  = (int) f;
-		}
-
-		public void SendOut()
-		{
-			pd.SendToOutlet(x, 0, new Atom(curr));
-		}
-
-		public void Sum(float f)
-		{
-			curr += (int) f;
-			pd.SendToOutlet(x, 0, new Atom(curr));
-		}
-
+	// this function MUST exist
+	public static void Main()
+	{
+        Post("Count.Main");       
 	}
+
+/*
+    public void MyBang() 
+    { 
+        Post("Count-BANG"); 
+    }
+
+    public void MyFloat(float f)
+    {
+        Post(String.Format("Count-FLOAT {0}",f));       
+    }
+*/    
+    protected override void MethodBang() 
+    { 
+        Post("Count-BANG"); 
+    }
+
+    protected override void MethodFloat(float f) 
+    { 
+        Post("Count-FLOAT "+f.ToString()); 
+    }
+
+    protected override void MethodSymbol(PureData.Symbol s) 
+    { 
+        Post("Count-SYMBOL "+s.ToString()); 
+    }
+
+    /*
+	public void Init(float f)
+	{
+		curr  = (int) f;
+	}
+
+	public void SendOut()
+	{
+		pd.SendToOutlet(x, 0, new Atom(curr));
+	}
+
+	public void Sum(float f)
+	{
+		curr += (int) f;
+		pd.SendToOutlet(x, 0, new Atom(curr));
+	}
+
+*/
 }
