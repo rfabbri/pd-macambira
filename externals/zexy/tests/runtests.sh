14e004971d1f5347e482ba248dcabdba1af1a593
zmoelnig <zmoelnig@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2006-01-30 09:12:44 +0000

    changed the lognames from "runtest.log.$$" to "log-runtests.<date>"
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@4521 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- externals/zexy/tests/runtests.sh
+++ externals/zexy/tests/runtests.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 
+#SUFFIX="$$"
+SUFFIX=$(date +%y%m%d-%H%M%S)
+
 RUNTESTS_TXT=runtests.txt
-RUNTESTS_LOG=runtests.log
+RUNTESTS_LOG=log-runtests.${SUFFIX}
 
 ls -1 */*.pd | sed 's/\.pd/;/' > ${RUNTESTS_TXT}
 
 LIBFLAGS="-lib ../zexy -path ../abs/"
 
 function run_nogui() {
- pd ${LIBFLAGS} -nogui runtests_nogui.pd > ${RUNTESTS_LOG}.$$ 2>&1 
+ pd ${LIBFLAGS} -nogui runtests_nogui.pd > ${RUNTESTS_LOG} 2>&1 
  NUMTESTS=`grep -c . ${RUNTESTS_TXT}`
- echo "regression-test: ${NUMTESTS} tests total" >>  ${RUNTESTS_LOG}.$$
+ echo "regression-test: ${NUMTESTS} tests total" >>  ${RUNTESTS_LOG}
  
- cat ${RUNTESTS_LOG}.$$ | egrep "^regression-test: " | sed -e 's/^regression-test: //'
+ cat ${RUNTESTS_LOG} | egrep "^regression-test: " | sed -e 's/^regression-test: //'
 }
 
 function run_withgui() {
