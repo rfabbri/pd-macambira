2f92936014fabb490c217034605c786506e88a3c
eighthave <eighthave@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2006-02-06 21:18:53 +0000

    added input_arrays.? files which are generated from ./make-arrays-from-input.h.pl since it makes building [hid] so much easier
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@4558 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

---
+++ externals/hcs/hid/input_arrays.h
@@ -0,0 +1,42 @@
+#ifndef _INPUT_ARRAYS_H
+#define _INPUT_ARRAYS_H
+
+
+char *ev[32];
+char *ev_syn[512];
+char *ev_key[512];
+char *ev_rel[16];
+char *ev_abs[64];
+char *ev_msc[8];
+char *ev_led[16];
+char *ev_snd[8];
+char *ev_rep[2];
+char *ev_ff[128];
+char *ev_pwr[1];
+char *ev_ff_status[2];
+char *ev_5[16];
+char *ev_6[16];
+char *ev_7[16];
+char *ev_8[16];
+char *ev_9[16];
+char *ev_10[16];
+char *ev_11[16];
+char *ev_12[16];
+char *ev_13[16];
+char *ev_14[16];
+char *ev_15[16];
+char *ev_16[16];
+char *ev_19[16];
+char *ev_24[16];
+char *ev_25[16];
+char *ev_26[16];
+char *ev_27[16];
+char *ev_28[16];
+char *ev_29[16];
+char *ev_30[16];
+char *ev_31[16];
+char **event_names[32];
+
+
+
+#endif  /* #ifndef _INPUT_ARRAYS_H */
