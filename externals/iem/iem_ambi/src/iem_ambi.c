bc92a6da64f6ef3dcc1f99e62008982888260c6b
zmoelnig <zmoelnig@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2006-03-09 17:38:37 +0000

    fixed build-system
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@4671 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- externals/iem/iem_ambi/src/iem_ambi.c
+++ externals/iem/iem_ambi/src/iem_ambi.c
@@ -11,6 +11,7 @@ iem_ambi written by Thomas Musil, Copyright (c) IEM KUG Graz Austria 2000 - 2005
 
 #include "m_pd.h"
 #include "iemlib.h"
+#include "iem_ambi_sources.h"
 
 static t_class *iem_ambi_class;
 
@@ -21,23 +22,11 @@ static void *iem_ambi_new(void)
 	return (x);
 }
 
-void ambi_encode_setup(void);
-void ambi_decode_setup(void);
-void ambi_decode2_setup(void);
-void ambi_decode3_setup(void);
-void ambi_decode_cube_setup(void);
-void ambi_rot_setup(void);
-
 /* ------------------------ setup routine ------------------------- */
 
 void iem_ambi_setup(void)
 {
-	ambi_encode_setup();
-	ambi_decode_setup();
-	ambi_decode2_setup();
-	ambi_decode3_setup();
-	ambi_decode_cube_setup();
-	ambi_rot_setup();
+	iem_ambi_sources_setup();
 
     post("iem_ambi (R-1.16) library loaded!   (c) Thomas Musil 05.2005");
 	post("   musil%ciem.at iem KUG Graz Austria", '@');
