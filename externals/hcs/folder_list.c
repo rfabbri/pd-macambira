3a5b12359e92136ac324d38c6396095a538e0815
eighthave <eighthave@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2006-03-27 03:16:14 +0000

    cleaned up files for release
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@4780 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- externals/hcs/folder_list.c
+++ externals/hcs/folder_list.c
@@ -1,3 +1,28 @@
+/* --------------------------------------------------------------------------*/
+/*                                                                           */
+/* object for getting file listings using wildcard patterns                  */
+/* Written by Hans-Christoph Steiner <hans@at.or.at>                         */
+/*                                                                           */
+/* Copyright (c) 2006 Hans-Christoph Steiner                                 */
+/*                                                                           */
+/* This program is free software; you can redistribute it and/or             */
+/* modify it under the terms of the GNU General Public License               */
+/* as published by the Free Software Foundation; either version 2            */
+/* of the License, or (at your option) any later version.                    */
+/*                                                                           */
+/* See file LICENSE for further informations on licensing terms.             */
+/*                                                                           */
+/* This program is distributed in the hope that it will be useful,           */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of            */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             */
+/* GNU General Public License for more details.                              */
+/*                                                                           */
+/* You should have received a copy of the GNU General Public License         */
+/* along with this program; if not, write to the Free Software Foundation,   */
+/* Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.           */
+/*                                                                           */
+/* --------------------------------------------------------------------------*/
+
 #include <m_pd.h>
 
 #ifdef _WIN32
@@ -9,7 +34,7 @@
 #include <glob.h>
 #endif
 
-static char *version = "$Revision: 1.7 $";
+static char *version = "$Revision: 1.8 $";
 
 #define DEBUG(x)
 //#define DEBUG(x) x 
