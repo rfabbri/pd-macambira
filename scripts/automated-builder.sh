065cd5d7293f415a6f4b0779531194baf3590bda
eighthave <eighthave@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2006-07-19 14:41:10 +0000

    changed up to use rsync on the build machines
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@5380 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- scripts/automated-builder.sh
+++ scripts/automated-builder.sh
@@ -2,13 +2,23 @@
 
 # this script is the first attempt to have an automated updater and builder
 
-cvs_root_dir=`echo $0 | sed 's|\(.*\)/.*$|\1|'`/..
+auto_build_root_dir=`echo $0 | sed 's|\(.*\)/.*$|\1|'`/..
 SYSTEM=`uname -s`
+DATE=`date +%Y-%m-%d`
+TIME=`date +%H.%M.%S`
+LOGFILE=/tmp/build-log_${SYSTEM}_${DATE}_${TIME}.txt
 
-cd "${cvs_root_dir}/packages"
-make unpatch_pd
-../scripts/update-developer-layout.sh
-make patch_pd
+# convert into absolute path
+cd ${auto_build_root_dir}
+auto_build_root_dir=`pwd`
+
+echo "root: $auto_build_root_dir" 
+
+cd ${auto_build_root_dir}/packages
+# let rsync handle the cleanup
+#make unpatch_pd
+
+rsync -av --delete rsync://128.238.56.50/pure-data/ ${auto_build_root_dir}/
 
 BUILD_DIR=.
 if [ "$SYSTEM" == "Linux" ]; then
@@ -20,10 +30,12 @@ if [ "$SYSTEM" == "Darwin" ]; then
 fi
 
 cd "$BUILD_DIR"
+pwd
 make distclean
 make package_clean
 rm -rf build
+#make -C "${auto_build_root_dir}/packages" patch_pd
 make install && make package
 
-#make test_locations
 #make test_package
+make test_locations
