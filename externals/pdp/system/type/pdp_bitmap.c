b06c4ae1042b69c94caf8d79b5153b2e505e730a
matju <matju@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2006-07-21 15:33:30 +0000

    fixed storage-class to make gcc happy
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@5392 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- externals/pdp/system/type/pdp_bitmap.c
+++ externals/pdp/system/type/pdp_bitmap.c
@@ -463,6 +463,11 @@ static int _pdp_packet_bitmap_convert_yv12_to_image(int packet, t_pdp_symbol *de
     return new_p;
 }
 
+static inline u8 _map(s32 pixel){
+    s32 mask = ~(pixel>>16);
+    return ((pixel >> 7) & mask);
+}
+
 static int _pdp_packet_bitmap_convert_mchp_to_rgb8(int packet, t_pdp_symbol *dest_template)
 {
     t_pdp *header = pdp_packet_header(packet);
@@ -474,11 +479,6 @@ static int _pdp_packet_bitmap_convert_mchp_to_rgb8(int packet, t_pdp_symbol *des
     int plane = w*h;
     int nb_channels = image->depth;
     int new_p, i;
-
-    static inline u8 _map(s32 pixel){
-	s32 mask = ~(pixel>>16);
-	return ((pixel >> 7) & mask);
-    }
 
     switch(nb_channels){
     default: return -1;
