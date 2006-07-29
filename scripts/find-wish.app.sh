dcbeedaa82442caa0113f0f9ab5f42771391358e
eighthave <eighthave@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2006-07-29 23:12:30 +0000

    fixed up logic for tracking down which Wish.app to use and whether to copy it has embedded Frameworks
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@5432 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- scripts/find-wish.app.sh
+++ scripts/find-wish.app.sh
@@ -4,11 +4,11 @@ cvs_root_dir=`echo $0 | sed 's|\(.*\)/.*$|\1|'`/..
 cd $cvs_root_dir
 cvs_root_dir=`pwd`
 
-TCLTK=`/bin/ls -1r ${cvs_root_dir}/packages/darwin_app/TclTkAquaStandalone-8.?.*.dmg|head -n 1|sed 's|.*/\(.*\)\.dmg|\1|'`
+TCLTK="$1"
 
 if [ "x${TCLTK}" != "x" ]; then
 	 test -d /Volumes/${TCLTK} || \
-		  hdiutil mount ${cvs_root_dir}/packages/darwin_app/${TCLTK}.dmg
+		  hdiutil mount -quiet ${cvs_root_dir}/packages/darwin_app/${TCLTK}.dmg
 	 echo "/Volumes/${TCLTK}"
 else
 	 if [ -d "/Library/Frameworks/Tk.framework/Resources" ]; then
@@ -20,5 +20,3 @@ else
 	 fi
 fi
 
-exit
-	 
