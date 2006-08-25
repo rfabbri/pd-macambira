005195fe7906a691fb20f210038f03a138f77c6d
doelie <doelie@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2006-08-25 23:07:37 +0000

    removed envelope_utils.c - moved milliseconds_2_one_minus_realpole() to extlib_util.h as static inline
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@5744 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- externals/creb/modules/extlib_util.h
+++ externals/creb/modules/extlib_util.h
@@ -31,7 +31,17 @@
 #define ENVELOPE_MIN   ENVELOPE_RANGE
 
 /* convert milliseconds to 1-p, with p a real pole */
-float milliseconds_2_one_minus_realpole(float time);
+static inline float milliseconds_2_one_minus_realpole(float time)
+{
+  float r;
+
+  if (time < 0.0f) time = 0.0f;
+  r = -expm1(1000.0f * log(ENVELOPE_RANGE) / (sys_getsr() * time));
+  if (!(r < 1.0f)) r = 1.0f;
+
+  //post("%f",r);
+  return r;
+}
 
 
 typedef union
