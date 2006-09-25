bf44c09e23ef7cc0a6936f3bd16add0868d1ab93
zmoelnig <zmoelnig@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2006-09-25 10:25:51 +0000

    delete *.c files before re-generating them (since else we get double entries in the files); LATER remove all .c files from this directory (they ought to be generated on the fly); check whether to use d_mayer_fft.c or d_fft_mayer.c
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@6019 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- externals/corelibs/generate.sh
+++ externals/corelibs/generate.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
 
+# delete all .c-files in order to not duplicate entries in there
+## LATER: ask hcs why he is using "touch" and ">>" instead of
+## an initial ">"
+rm -f lib_?_*.c
+
 # put these at the top of the file
 touch lib_d_fft.c
 echo '#include "../../pd/src/d_fftroutine.c"' >> lib_d_fft.c
-echo '#include "../../pd/src/d_fft_mayer.c"' >> lib_d_fft.c
+if [ -e "../../pd/src/d_fft_mayer.c" ]
+then
+ echo '#include "../../pd/src/d_fft_mayer.c"' >> lib_d_fft.c
+else
+ echo '#include "../../pd/src/d_mayer_fft.c"' >> lib_d_fft.c
+fi
+
 
 for file in ../../pd/src/[dx]_*.c; do 
 	 newfile=`echo $file | sed 's|.*/src/\([dx]_\)|lib_\1|'`
@@ -18,6 +29,6 @@ for file in ../../pd/src/[dx]_*.c; do
 done
 
 # these files hold code for other classes, but no classes
-rm lib_d_fftroutine.c lib_d_fft_mayer.c lib_d_resample.c
+rm -f lib_d_fftroutine.c lib_d_fft_mayer.c lib_d_mayer_fft.c lib_d_resample.c
 
 
