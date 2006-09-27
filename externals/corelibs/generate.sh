97f75e9b956a8f75376c2e5f25ce2b04a4821642
zmoelnig <zmoelnig@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2006-09-27 07:04:59 +0000

    reverted the shit to how it used to be. don't ever touch it again
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@6034 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- externals/corelibs/generate.sh
+++ externals/corelibs/generate.sh
@@ -1,20 +1,9 @@
 #!/bin/sh
 
-# delete all .c-files in order to not duplicate entries in there
-## LATER: ask hcs why he is using "touch" and ">>" instead of
-## an initial ">"
-rm -f lib_?_*.c
-
 # put these at the top of the file
 touch lib_d_fft.c
 echo '#include "../../pd/src/d_fftroutine.c"' >> lib_d_fft.c
-if [ -e "../../pd/src/d_fft_mayer.c" ]
-then
- echo '#include "../../pd/src/d_fft_mayer.c"' >> lib_d_fft.c
-else
- echo '#include "../../pd/src/d_mayer_fft.c"' >> lib_d_fft.c
-fi
-
+echo '#include "../../pd/src/d_mayer_fft.c"' >> lib_d_fft.c
 
 for file in ../../pd/src/[dx]_*.c; do 
 	 newfile=`echo $file | sed 's|.*/src/\([dx]_\)|lib_\1|'`
@@ -29,6 +18,6 @@ for file in ../../pd/src/[dx]_*.c; do
 done
 
 # these files hold code for other classes, but no classes
-rm -f lib_d_fftroutine.c lib_d_fft_mayer.c lib_d_mayer_fft.c lib_d_resample.c
+rm lib_d_fftroutine.c lib_d_mayer_fft.c lib_d_resample.c
 
 
