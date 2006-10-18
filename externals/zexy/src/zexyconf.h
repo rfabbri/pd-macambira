396c55432511e26cacc089dd8fee085c324c40f0
zmoelnig <zmoelnig@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2006-10-18 08:29:53 +0000

    this file got updated accidentially; in this version (which shouldn't change), nothing optional is defined, so it should allow to build zexy on any platform (with less features)
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@6131 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- externals/zexy/src/zexyconf.h
+++ externals/zexy/src/zexyconf.h
@@ -1,18 +1,26 @@
-/* zexyconf.h.  Generated from zexyconf.h.in by configure.  */
+/* this is a zexyconf.h for windows 
+ * on unix-derivatives (linux, os-X,...) this file should be overwritten by configure
+ * via the template zexyconf.h.in
+ *
+ * if you cannot use configure to re-generate this file, make sure all the defines 
+ * are set correctly
+ */
+
 
 /* Define if you have the <regex.h> header file.  */
-#define HAVE_REGEX_H 1
+/* #undef HAVE_REGEX_H */
 
 /* Define if you have the <alloca.h> header file.  */
-#define HAVE_ALLOCA_H 1
+/* #undef HAVE_ALLOCA_H */
 
 /* define if you want parallelport-support (direct access to the port address) */
-#define Z_WANT_LPT 1
+/* most likely, you do not want to do this on apple hardware */
+/* #define Z_WANT_LPT 1 */
 
 /* define if you have the <linux/ppdev.h> header file.
  * (for parport _device_ support) 
  * you need Z_WANT_LPT for this to have an effect ! 
  */
-#define HAVE_LINUX_PPDEV_H 1
+/* #undef HAVE_LINUX_PPDEV_H */
 
 
