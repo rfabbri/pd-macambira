aaf4359a6052c43061a0b669e8d7b72b858804c4
eighthave <eighthave@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2006-11-20 00:51:30 +0000

    first functional example, it worked in the test at least, time to test some more
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@6328 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

---
+++ externals/loaders/libdir.c
@@ -0,0 +1,93 @@
+#include "m_pd.h"
+#include "s_stuff.h"
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+static char *version = "$Revision: 1.1 $";
+
+/* This loader opens a directory as a library.  In the long run, the idea is
+ * that one folder will have all of objects files, all of the related
+ * *-help.pd files, a file with meta data for the help system, etc.  Then to
+ * install the lib, it would just be dropped into extra, or the path anywhere.
+ *
+ * Ultimately, the meta file will be read for meta data, specifically for
+ * the auto-generated Help system, but for other things too.  Right now,
+ * its just used as a marker that a directory is meant to be a library.
+ * Plus its much easier to implement it this way, I can use
+ * open_via_path() instead of writing a new function.  The grand plan is
+ * to have one directory hold the objects, help files, manuals,
+ * etc. making it a self-contained library. <hans@at.or.at>
+ */
+
+static int libdir_loader(t_canvas *canvas, char *classname)
+{
+    int fd = -1;
+    char searchpathname[MAXPDSTRING], helppathname[MAXPDSTRING], 
+        fullclassname[MAXPDSTRING], dirbuf[MAXPDSTRING], *nameptr;
+    
+    post("libdir_loader classname: %s\n", classname);
+
+    /* look for meta file (classname)/(classname).pd <hans@at.or.at> */
+    /* TODO: at "-META" to the meta filename */
+    strncpy(fullclassname, classname, MAXPDSTRING - 6);
+    strcat(fullclassname, "/");
+    strncat(fullclassname, classname, MAXPDSTRING - strlen(fullclassname) - 6);
+    strncat(fullclassname, classname, MAXPDSTRING - strlen(fullclassname) - 6);
+    strcat(fullclassname, "-meta");
+/*    if ((fd = open_via_path(dirname, fullclassname, ".pd",
+	  dirbuf, &nameptr, MAXPDSTRING, 1)) < 0) */
+    post("libdir_loader fullclassname: %s\n", fullclassname);
+
+
+// TODO: this needs to be figured out! its the new 0.40 way of doing things
+/* send NULL as the canvas for the first argument, and it'll only look in the
+ * global path and "." */
+    if ((fd = canvas_open(canvas, fullclassname, ".pd",
+        dirbuf, &nameptr, MAXPDSTRING, 1)) < 0)
+
+    {
+        return (0);
+    }
+    close(fd);
+
+    post("libdir_loader loaded fullclassname: %s\n", fullclassname);
+    
+        /* create full path to libdir for adding to the paths */
+    strcpy(searchpathname,dirbuf);
+//    strcat(searchpathname,"/");
+//    strncat(searchpathname,classname, MAXPDSTRING-strlen(searchpathname));
+
+    strncpy(helppathname, sys_libdir->s_name, MAXPDSTRING-30);
+    helppathname[MAXPDSTRING-30] = 0;
+    strcat(helppathname, "/doc/5.reference/");
+    strcat(helppathname, classname);
+
+    sys_searchpath = namelist_append_files(sys_searchpath, searchpathname);
+    /* this help path supports having the help files in a complete library
+     * directory format, where everything is in one folder.  The help meta
+     * system needs to be implemented for this to work with the new Help
+     * menu/browser system. Ultimately, /path/to/extra will have to be added
+     * to sys_helppath in order for this to work properly.<hans@at.or.at> */
+    sys_helppath = namelist_append_files(sys_helppath, searchpathname);
+    
+        /* this should be changed to use sys_debuglevel */
+    if (sys_verbose) 
+    {
+        post("Added to search path: %s", searchpathname);
+        post("Added to help path: %s", searchpathname);
+        post("Added to help path: %s", helppathname);
+    }
+    if (sys_verbose) 
+        post("Loaded libdir %s from %s", classname, dirbuf);
+
+    return (1);
+}
+
+void libdir_setup(void)
+{
+    sys_register_loader(libdir_loader);
+    post("libdir loader %s",version);  
+    post("\twritten by Hans-Christoph Steiner <hans@at.or.at>");
+    post("\tcompiled on "__DATE__" at "__TIME__ " ");
+}
