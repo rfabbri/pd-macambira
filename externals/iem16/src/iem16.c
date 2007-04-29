c1dba5674d1e8b5e45e937b935136c5423ffa196
zmoelnig <zmoelnig@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2007-04-29 10:58:25 +0000

    disable the [iem16] object since it crashes my wine
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@7619 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- externals/iem16/src/iem16.c
+++ externals/iem16/src/iem16.c
@@ -9,15 +9,16 @@
 /* do a little help thing */
 
 typedef struct iem16 {
-  t_object t_ob;
+  t_object x_obj;
 } t_iem16;
 
-t_class *iem16_class;
+static t_class *iem16_class;
 
-void *iem16_new(void){
+
+static void *iem16_new(void){
   t_iem16 *x = (t_iem16 *)pd_new(iem16_class);
   post("iem16: 16bit objects for low memory usage");
-  return (void *)x;
+  return(x);
 }
 
 /* include some externals */
@@ -27,6 +28,12 @@ void iem16_array_tilde_setup();
 void iem16_delay_setup();
 
 void iem16_setup(void) {
+  static unsigned int setupcount=0;
+  if(setupcount>0) {
+    post("iem16:\tsetup called several times, skipping...");
+    return;
+  }
+  setupcount++;
   iem16_table_setup();
   iem16_array_setup();
   iem16_array_tilde_setup();
@@ -37,14 +44,17 @@ void iem16_setup(void) {
   post("iem16:\t(l) forum::für::umläute\t\tIOhannes m zmölnig");
   post("iem16:\tInstitute of Electronic Music and Acoustics, Graz - iem");
   post("iem16:\tcompiled: "__DATE__);
-  
-
+#if defined __WIN32__ || defined __WIN32
+  post("iem16:\ton W32 you cannot create the [iem16] object. nevermind...");
+#else
   iem16_class = class_new(gensym("iem16"), 
 			  iem16_new, 
 			  0, 
-			  sizeof(t_iem16), CLASS_NOINLET, A_NULL);
+			  sizeof(t_iem16), 0, 0);
+
   class_addcreator((t_newmethod)iem16_new, 
-		   gensym("IEM16"), A_NULL); 
+		   gensym("IEM16"), 0); 
+#endif
 }
 
 void IEM16_setup(void){
