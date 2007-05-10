038b78c6d65cbb1436ee4617fce428e135a9c7c4
zmoelnig <zmoelnig@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2007-05-10 13:57:56 +0000

    allow to query multiple values at once
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@7649 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- externals/zexy/src/packel.c
+++ externals/zexy/src/packel.c
@@ -34,19 +34,27 @@ static t_class *packel_class;
 typedef struct _packel
 {
   t_object x_obj;
-  t_float position;
+  t_float *position;
+  int count;
+
+  t_inlet**x_inlet;
+  t_outlet**x_outlet;
+
+  int x_warningflag;
 } t_packel;
 
-static void packel_list(t_packel *x, t_symbol *s, int argc, t_atom *argv)
-{
-  int mypos = x->position;
 
-  if (mypos) {
+static void packel_outelement(t_packel*x, int id, t_symbol*s,int argc, t_atom*argv)
+{
+  t_outlet*out=x->x_outlet[id];
+  int index= x->position[id];
+ 
+  if (index) {
     t_atom *current;
-    int pos = (mypos < 0)?(argc+mypos):(mypos-1);
+    int pos = (index < 0)?(argc+index):(index-1);
 
     if(argc==0){
-      if (pos==0||pos==-1)outlet_bang(x->x_obj.ob_outlet);
+      if (pos==0||pos==-1)outlet_bang(out);
       return;
     }
     
@@ -55,27 +63,31 @@ static void packel_list(t_packel *x, t_symbol *s, int argc, t_atom *argv)
     current = &(argv[pos]);
 
     switch (current->a_type) {
-    case A_FLOAT:
-      outlet_float(x->x_obj.ob_outlet, atom_getfloat(current));
-      break;
-    case A_SYMBOL:
-      outlet_symbol(x->x_obj.ob_outlet, atom_getsymbol(current));
-      break;
-    case A_POINTER:
-      outlet_pointer(x->x_obj.ob_outlet, current->a_w.w_gpointer);
-      break;
     case A_NULL:
-      outlet_bang(x->x_obj.ob_outlet);
+      outlet_bang(out);
     default:
-	  ;
+      outlet_list(out, gensym("list"), 1, current);
     }
-  } else outlet_list(x->x_obj.ob_outlet, s, argc, argv);
+  } else outlet_list(out, s, argc, argv); 
+}
+
+static void packel_list(t_packel *x, t_symbol *s, int argc, t_atom *argv)
+{
+  int c=x->count;
+  while(--c>=0) {
+    packel_outelement(x, c, s, argc, argv);
+  }
 }
 
 static void packel_anything(t_packel *x, t_symbol *s, int argc, t_atom *argv)
 {
   t_atom *av2 = (t_atom *)getbytes((argc + 1) * sizeof(t_atom));
   int i;
+
+  if(x->x_warningflag){
+    pd_error(x, "deprecation warning: you should only use lists for list data");
+    x->x_warningflag=0;
+  }
 
   for (i = 0; i < argc; i++)
     av2[i + 1] = argv[i];
@@ -84,20 +96,57 @@ static void packel_anything(t_packel *x, t_symbol *s, int argc, t_atom *argv)
   freebytes(av2, (argc + 1) * sizeof(t_atom));
 }
 
-static void *packel_new(t_floatarg f)
+
+static void packel_free(t_packel *x)
+{
+  int i=0;
+
+  for(i=0; i<x->count; i++) {
+    if(x->x_inlet &&x->x_inlet [i])inlet_free (x->x_inlet [i]);
+    if(x->x_outlet&&x->x_outlet[i])outlet_free(x->x_outlet[i]);
+  }
+
+  if(x->position)freebytes(x->position, x->count*sizeof(t_float));
+  if(x->x_inlet)   freebytes(x->x_inlet, x->count*sizeof(t_inlet*));
+  if(x->x_outlet)  freebytes(x->x_outlet, x->count*sizeof(t_outlet*));
+
+}
+
+
+static void *packel_new(t_symbol*s, int argc, t_atom*argv)
 {
   t_packel *x = (t_packel *)pd_new(packel_class);
-  outlet_new(&x->x_obj, 0);
-  floatinlet_new(&x->x_obj, &x->position);
-  x->position = (int) f;
+  
+  x->count=(argc>0)?argc:1;
+
+  x->position=(t_float*)getbytes(x->count*sizeof(t_float));
+  x->x_inlet=(t_inlet**)getbytes(x->count*sizeof(t_inlet*));
+  x->x_outlet=(t_outlet**)getbytes(x->count*sizeof(t_outlet*));
+
+  if(argc<1) {
+    x->position[0]=0.f;
+    x->x_inlet[0]=floatinlet_new(&x->x_obj, x->position);
+    x->x_outlet[0]=outlet_new(&x->x_obj, 0);
+  } else {
+    int i=0;
+    for(i=0; i<x->count; i++) {
+      x->position[i]=atom_getfloat(argv+i);
+      x->x_inlet   [i]=floatinlet_new(&x->x_obj, x->position+i);
+      x->x_outlet  [i]=outlet_new(&x->x_obj, 0);
+    }
+  }
+  x->x_warningflag=1;
+
 
   return (x);
 }
 
 void packel_setup(void)
 {
-  packel_class = class_new(gensym("packel"), (t_newmethod)packel_new, 
-			   0, sizeof(t_packel), 0, A_DEFFLOAT, 0);
+  packel_class = class_new(gensym("packel"), 
+                           (t_newmethod)packel_new, (t_method)packel_free, 
+                           sizeof(t_packel), 0,
+                           A_GIMME, 0);
 
   class_addlist  (packel_class, packel_list);
   class_addanything(packel_class, packel_anything);
