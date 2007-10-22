6ed9ad8da50fbe13e18845a1e6125d820097a1dd
eighthave <eighthave@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2007-10-22 22:41:47 +0000

    ok, this should work now, so switched to pd-cvs for recipient
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@8862 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- scripts/auto-build/mail-mingw-log-hack.sh
+++ scripts/auto-build/mail-mingw-log-hack.sh
@@ -12,7 +12,6 @@ for logfile in `ls -1 /var/www/auto-build/${DATE}/logs/${DATE}_*_mingw*.txt`; do
 	 completion_test=`tail -1 ${logfile}`
 	 if [ "x${completion_test}" != "xSUCCESS" ]; then
 		  SUBJECT="autobuild: $distro $HOSTNAME $DATE $TIME"
-#		  tail -20 ${logfile} | mail -s "autobuild: $logfile" ${RECIPIENT}
-		  tail -20 ${logfile} | mail -s "autobuild: $logfile" hans@eds.org
+		  tail -20 ${logfile} | mail -s "autobuild: $logfile" ${RECIPIENT}
 	 fi
 done
