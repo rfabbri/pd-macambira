e31d800603622b34572511d8c9b7a36ff1170353
eighthave <eighthave@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2007-10-22 21:47:57 +0000

    fixed path to logs
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@8861 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- scripts/auto-build/mail-mingw-log-hack.sh
+++ scripts/auto-build/mail-mingw-log-hack.sh
@@ -8,7 +8,7 @@ SCRIPT_DIR=`echo $0 | sed 's|\(.*\)/.*$|\1|'`
 # the name of this script
 SCRIPT=`echo $0| sed 's|.*/\(.*\)|\1|g'`
 
-for logfile in `ls -1 /home/pd/logs/${DATE}_*_mingw*.txt`; do
+for logfile in `ls -1 /var/www/auto-build/${DATE}/logs/${DATE}_*_mingw*.txt`; do
 	 completion_test=`tail -1 ${logfile}`
 	 if [ "x${completion_test}" != "xSUCCESS" ]; then
 		  SUBJECT="autobuild: $distro $HOSTNAME $DATE $TIME"
