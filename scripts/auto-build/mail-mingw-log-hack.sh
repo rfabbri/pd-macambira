a188d2c7c2d53586203ee2e9794abc1b95cd089c
zmoelnig <zmoelnig@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2007-11-27 20:49:22 +0000

    only send emails if RECIPIENT is defined
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@9056 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- scripts/auto-build/mail-mingw-log-hack.sh
+++ scripts/auto-build/mail-mingw-log-hack.sh
@@ -11,7 +11,9 @@ SCRIPT=`echo $0| sed 's|.*/\(.*\)|\1|g'`
 for logfile in `ls -1 /var/www/auto-build/${DATE}/logs/${DATE}_*_mingw*.txt`; do
 	 completion_test=`tail -1 ${logfile}`
 	 if [ "x${completion_test}" != "xSUCCESS" ]; then
+		if [ "x${RECIPIENT}" != "x" ]; then
 		  SUBJECT="autobuild: $distro $HOSTNAME $DATE $TIME"
 		  tail -20 ${logfile} | mail -s "autobuild: $logfile" ${RECIPIENT}
+		fi
 	 fi
 done
