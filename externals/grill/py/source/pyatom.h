4fc9f87d292cd9f2de9820520adfaffd7334ea67
xovo <xovo@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2008-01-03 16:21:04 +0000

    cross-platform fixes enable compiled-only scripts (without .py) use PyGILState_\*() functionality (enabled with PY_USE_GIL) fixes for non-GIL usage improved symbol comparison buffer protocol adapted for Python 2.5 enable module packages (module/__init__.py[co]), now also for Max
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@9120 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- externals/grill/py/source/pyatom.h
+++ externals/grill/py/source/pyatom.h
@@ -1,11 +1,13 @@
 /* 
-
 py/pyext - python script object for PD and Max/MSP
 
-Copyright (c)2002-2005 Thomas Grill (gr@grrrr.org)
+Copyright (c)2002-2008 Thomas Grill (gr@grrrr.org)
 For information on usage and redistribution, and for a DISCLAIMER OF ALL
 WARRANTIES, see the file, "license.txt," in this distribution.  
 
+$LastChangedRevision: 26 $
+$LastChangedDate: 2008-01-03 17:20:03 +0100 (Thu, 03 Jan 2008) $
+$LastChangedBy: thomas $
 */
 
 #include "main.h"
