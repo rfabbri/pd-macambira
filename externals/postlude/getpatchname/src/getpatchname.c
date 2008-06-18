7172d978f1749da115d9396080af4e448a6eee85
lukeiannini <lukeiannini@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2008-06-18 21:06:16 +0000

    getpatchname parent query capability and helpfile to match
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@10037 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- externals/postlude/getpatchname/src/getpatchname.c
+++ externals/postlude/getpatchname/src/getpatchname.c
@@ -1,4 +1,3 @@
-
 /* getpatchname - Returns the filename of the current patch 
  * 
  * Copyright (C) 2006 Jamie Bullock 
@@ -27,28 +26,38 @@ typedef struct _getpatchname {
   t_object  x_obj;
   t_symbol *patch_name;
   t_outlet *outlet;
+  t_canvas *x_canvas;
+  int x_level;
 } t_getpatchname;
 
 void getpatchname_bang(t_getpatchname *x)
 {
 /* At some point we need to be to get the new patch name if it changes, couldn't make this work though */
-    outlet_symbol(x->outlet, x->patch_name);
+    int i = x->x_level;
+    t_canvas* last = x->x_canvas;
+    
+    while (i>0) {
+        i--;
+        if (last->gl_owner) last = last->gl_owner;
+    }
+    
+    outlet_symbol(x->outlet, last->gl_name);
 }
 
-void *getpatchname_new(void)
+void *getpatchname_new(t_floatarg level)
 {
   t_getpatchname *x = (t_getpatchname *)pd_new(getpatchname_class);
   x->patch_name = canvas_getcurrent()->gl_name;
+  x->x_canvas = canvas_getcurrent();
   x->outlet = outlet_new(&x->x_obj, &s_symbol);
+  x->x_level = level;
   return (void *)x;
 }
 
 void getpatchname_setup(void) {
   getpatchname_class = class_new(gensym("getpatchname"),
         (t_newmethod)getpatchname_new,
-        0, sizeof(t_getpatchname),
-        CLASS_DEFAULT, 0);
+        0, sizeof(t_getpatchname), 0,
+        A_DEFFLOAT, 0);
   class_addbang(getpatchname_class, getpatchname_bang);
 }
-
-
