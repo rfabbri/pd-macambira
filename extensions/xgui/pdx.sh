9e30de890e5d38635b0581a15c606b8b09d28aa1
zmoelnig <zmoelnig@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2008-06-19 13:54:38 +0000

    fixed svn:executable flag; use env-var PD for determining which pd to use in pdx
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@10051 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- extensions/xgui/pdx.sh
+++ extensions/xgui/pdx.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+if test "x${PD}" = "x"; then
+ PD=../bin/pd
+fi
 ./bin/xgui.sh &
-../bin/pd -open ./main/pdx_connect.pd \
+${PD} -open ./main/pdx_connect.pd \
  -path ./main/ \
  -path ./adapters_in \
  -path ./adapters_out \
