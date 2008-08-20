dae610b4daa00a0786580ff6baab534f778bb5a6
zmoelnig <zmoelnig@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2008-08-20 12:17:25 +0000

    Gem is now also under control of SVN
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@10257 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- scripts/checkout-developer-layout.sh
+++ scripts/checkout-developer-layout.sh
@@ -7,8 +7,9 @@
 #	 - with no arguments, it will check out the code using anonymous CVS.
 #	 - to check out using your SourceForge ID, add that as the argument
 
-# Be aware that SourceForge's anonymous CVS server is generally 24 hours
-# behind the authenticated CVS.
+URL="https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk/"
+GEMURL="https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/"
+PDAUTH=""
 
 print_usage ()
 {
@@ -19,28 +20,27 @@ print_usage ()
     exit
 }
 
-URL="https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk/"
-
 if [ $# -eq 0 ]; then
     echo "Checking out anonymously. Give your SourceForge ID if you don't want that."
-	svn checkout $URL pure-data
 elif [ "$1" == "--help" ]; then
     print_usage
 elif [ "$1" == "-h" ]; then
     print_usage
 elif [ $# -eq 1 ]; then
-    svn checkout --username $1 $URL pure-data
+    PDAUTH="--username $1"
 else
     print_usage
 fi
 
+echo "checking out pure-data"
+svn checkout $PDAUTH $URL pure-data
+
 cd pure-data
 
-# Gem is still separate
-echo -e "\n\n The password to the Gem anonymous CVS access is blank, so just press Enter\n"
-export CVSROOT=:pserver:anonymous@cvs.gem.iem.at:/cvsroot/pd-gem
-cvs login
-cvs checkout Gem GemLibs
+for section in Gem videoIO; do
+         echo "checking out Gem::${section}"
+         svn checkout ${PDAUTH} ${GEMURL}/${section} ${section}
+done
 
 
 # make the symlinks which simulate the files being installed into the packages
