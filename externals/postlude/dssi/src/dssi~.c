1062ff4853921883fac5d7935ead48752cc84275
postlude <postlude@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2009-03-07 18:16:06 +0000

    - Fixed loader bug. Now loading SYSEX patches uses Pd search path not absolute path
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@10842 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- externals/postlude/dssi/src/dssi~.c
+++ externals/postlude/dssi/src/dssi~.c
@@ -1607,7 +1607,7 @@ static t_int dssi_tilde_dssi_methods(t_dssi_tilde *x, t_symbol *s, int argc, t_a
                     strcpy(key, "patches0");
 
                     /* FIX: duplicates code from load_plugin() */
-                    fd = canvas_open(x->x_canvas, "", "",
+                    fd = canvas_open(x->x_canvas, filename, "",
                             mydir, &filename, MAXPDSTRING, 0);
 
                     if(fd >= 0){
@@ -1615,9 +1615,12 @@ static t_int dssi_tilde_dssi_methods(t_dssi_tilde *x, t_symbol *s, int argc, t_a
                         pathlen = strlen(mydir);
                         temp = &mydir[pathlen];
                         sprintf(temp, "/%s", filename);
-                        post("%s", filepath);
+                        //post("%s", filepath);
+                        fp = fopen(filepath, "rb");
                         return 0;
-                        /* fp = fopen(filepath, "rb");  */
+                    }
+                    else{
+                        post("dssi~: unable to get file descriptor");
                     }
 
                     /*From dx7_voice_data by Sean Bolton */
