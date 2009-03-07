c535cb15a84ab2df1c1874f63b995820b5ee0ae9
mukau <mukau@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2009-03-07 19:51:54 +0000

    + updated 'hello' example external: eliminate build-time dependence on svn:special symlinks
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@10844 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- externals/moocow/hello/hello.c
+++ externals/moocow/hello/hello.c
@@ -24,7 +24,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *=============================================================================*/
 #include <m_pd.h>
-#include "mooPdUtils.h"
+#include "common/mooPdUtils.h"
 
 #ifdef HAVE_CONFIG_H
 # include "config.h"
---
+++ externals/moocow/hello/prepare-svn.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+USAGE="$0 {copy|link} [COMMON_SRC=../common] [COMMON_DST=./common]"
+
+if test -n "$1"; then
+  case "$1" in
+    copy)
+      CP="cp -a";
+      ;;
+    link)
+      CP="ln -s";
+      ;;
+    *)
+      echo "Usage: $USAGE"
+      exit 1;
+      ;;
+  esac
+else
+  CP="ln -s"
+fi
+
+if test -n "$2"; then
+  COMMON_SRC="$2";
+else
+  COMMON_SRC="../common";
+fi
+
+if test -n "$3"; then
+  COMMON_DST="$3";
+else
+  COMMON_DST="./common";
+fi
+
+##-- copy or link in common dir
+rm -rf "$COMMON_DST";
+$CP "$COMMON_SRC" "$COMMON_DST";
