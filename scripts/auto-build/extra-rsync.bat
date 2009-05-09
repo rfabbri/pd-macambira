ca09546520c275c1067ed60713034496fbb16daa
eighthave <eighthave@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2009-05-09 17:04:52 +0000

    added the --whole-file option to rsync, which seems to eliminate the problems with rsync stalling out after 5-10 files
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@11308 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- scripts/auto-build/extra-rsync.bat
+++ scripts/auto-build/extra-rsync.bat
@@ -6,8 +6,8 @@ REM C:\msys\1.0\bin\sh --login -i -c find /home/pd/auto-build/pd-extended/ -name
 
 REM Cygwin rsync seems to be unhappy with SVN's .svn file permissions, so
 REM ignore SVN files first to get all the 'meat'
-rsync -av --progress --exclude='*inv\**' --cvs-exclude --timeout=60 rsync://128.238.56.50/distros/pd-extended/ /home/pd/auto-build/pd-extended/
+rsync -av --progress --whole-file --exclude='*inv\**' --cvs-exclude --timeout=60 rsync://128.238.56.50/distros/pd-extended/ /home/pd/auto-build/pd-extended/
 sleep 60
 REM now get the SVN changes, this might fail a lot, especially on '.svn/entries'
-rsync -av --progress --exclude='*inv\**' --timeout=60 rsync://128.238.56.50/distros/pd-extended/ /home/pd/auto-build/pd-extended/
+rsync -av --progress --whole-file --exclude='*inv\**' --timeout=60 rsync://128.238.56.50/distros/pd-extended/ /home/pd/auto-build/pd-extended/
 
