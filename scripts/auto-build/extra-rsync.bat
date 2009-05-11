133df5ff8770b7eee8d3764c7847d9eff502c104
eighthave <eighthave@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2009-05-11 21:50:13 +0000

    added cacls command to make sure perms are workable... a windows, what a pain...
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@11317 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- scripts/auto-build/extra-rsync.bat
+++ scripts/auto-build/extra-rsync.bat
@@ -1,13 +1,17 @@
-REM @echo off
-
 cd \msys\1.0\home\pd\
 
-REM C:\msys\1.0\bin\sh --login -i -c find /home/pd/auto-build/pd-extended/ -name entries 
+REM reset perms
+echo y|cacls c:\msys\1.0\home\pd\auto-build /C /T /G pd:F everyone:R 
 
 REM Cygwin rsync seems to be unhappy with SVN's .svn file permissions, so
 REM ignore SVN files first to get all the 'meat'
 rsync -av --progress --whole-file --exclude='*inv\**' --cvs-exclude --timeout=60 rsync://128.238.56.50/distros/pd-extended/ /home/pd/auto-build/pd-extended/
+
 sleep 60
+
 REM now get the SVN changes, this might fail a lot, especially on '.svn/entries'
 rsync -av --progress --whole-file --exclude='*inv\**' --timeout=60 rsync://128.238.56.50/distros/pd-extended/ /home/pd/auto-build/pd-extended/
+
+REM reset perms
+echo y|cacls c:\msys\1.0\home\pd\auto-build /C /T /G pd:F everyone:R 
 
