31d4eda9db6b1b30a2cfec19375eefe457b07eb5
eighthave <eighthave@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2009-05-14 04:35:10 +0000

    more fun with fighting against windows permission madness
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@11354 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- scripts/auto-build/extra-rsync.bat
+++ scripts/auto-build/extra-rsync.bat
@@ -1,7 +1,10 @@
+REM uses http://setacl.sourceforge.net/
+
 cd \msys\1.0\home\pd\
 
-REM reset perms
-echo y|cacls c:\msys\1.0\home\pd\auto-build /C /T /G pd:F everyone:R 
+REM echo y|cacls c:\msys\1.0\home\pd\auto-build /C /T /G pd:F everyone:R 
+
+setacl -on c:\msys\1.0\home\pd\auto-build\pd-extended -ot file -actn ace -ace "n:pd;p:full,write_owner;i:so,sc;m:set" -ace "n:everyone;p:read;i:so,sc;m:set"
 
 REM Cygwin rsync seems to be unhappy with SVN's .svn file permissions, so
 REM ignore SVN files first to get all the 'meat'
@@ -11,7 +14,3 @@ sleep 60
 
 REM now get the SVN changes, this might fail a lot, especially on '.svn/entries'
 rsync -av --progress --whole-file --exclude='*inv\**' --timeout=60 rsync://128.238.56.50/distros/pd-extended/ /home/pd/auto-build/pd-extended/
-
-REM reset perms
-echo y|cacls c:\msys\1.0\home\pd\auto-build /C /T /G pd:F everyone:R 
-
