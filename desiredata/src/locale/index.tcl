23b28bd1fe4a64886cd09896de0e5273adcbbd4c
matju <matju@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2009-05-17 04:53:51 +0000

    adding language "auto" in the GUI
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@11421 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- desiredata/src/locale/index.tcl
+++ desiredata/src/locale/index.tcl
@@ -4,7 +4,7 @@ say deutsch    "Deutsch"
 say catala     "Català"
 say espanol    "Español"
 say portugues  "Português"
-say brasileiro "Português Do Brasil"
+say brasileiro "Português do Brasil"
 say italiano   "Italiano"
 say euskara    "Euskara"
 say bokmal     "Norsk Bokmål"
@@ -24,4 +24,28 @@ set ::langoptions {
 	english francais deutsch catala espanol portugues brasileiro
 	italiano euskara bokmal dansk nederlands turkce polski russkij chinese nihongo
 	
+}
+
+proc figure_out_language {language} {
+ switch -regexp -- $language {
+  ^(en|english)$      {list iso8859-1 english}
+  ^(fr|francais)$     {list iso8859-1 francais}
+  ^(de|deutsch)$      {list iso8859-1 deutsch}
+  ^(ca|catala)$       {list iso8859-1 catala}
+  ^(es|espanol)$      {list iso8859-1 espanol}
+  ^(pt|portugues)$    {list iso8859-1 portugues}
+  ^(it|italiano)$     {list iso8859-1 italiano}
+  ^(nb|bokmal)$       {list iso8859-1 bokmal}
+  ^(ch|chinese)$      {list utf-8     chinese}
+  ^(eu|euskara)$      {list iso8859-1 euskara}
+  ^(eo|esperanto)$    {list utf-8     esperanto}
+  ^(pl|polski)$       {list utf-8     polski}
+  ^(dk|dansk)$        {list iso8859-1 dansk}
+  ^(ja|nihongo)$      {list iso8859-1 nihongo}
+  ^(br|brasileiro)$   {list iso8859-1 brasileiro}
+  ^(tr|turkce)$       {list utf-8     turkce}
+  ^(nl|nederlands)$   {list iso8859-1 nederlands}
+  ^(ru|russkij)$      {list utf-8     russkij}
+  default {error "unknown language: $language"}
+ }
 }
