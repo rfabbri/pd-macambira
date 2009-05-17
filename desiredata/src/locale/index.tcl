1c8d83ec0e806db1692ba3b53b3341209bd50cef
matju <matju@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2009-05-17 03:22:00 +0000

    move language list to where it belongs... but desire.tk still has several language lists... sorry
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@11418 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- desiredata/src/locale/index.tcl
+++ desiredata/src/locale/index.tcl
@@ -1,21 +1,27 @@
 say english    "English"
 say francais   "Français"
-say espanol    "Español"
 say deutsch    "Deutsch"
-say bokmal     "Norsk Bokmål"
-say italiano   "Italiano"
+say catala     "Català"
+say espanol    "Español"
 say portugues  "Português"
 say brasileiro "Português Do Brasil"
-say catala     "Català"
+say italiano   "Italiano"
 say euskara    "Euskara"
-say polski     "Polski"
+say bokmal     "Norsk Bokmål"
 say dansk      "Dansk"
 say nederlands "Nederlands"
+say polski     "Polski"
 say turkce     "Türkçe"
 
 # those were made using:
 # iconv -f utf-8 -t ucs-2 | od -tx2 -An | sed 's/ /\\u/g'
 
+say russkij "\u0420\u0443\u0441\u0441\u043a\u0438\u0439"
 say chinese "\u4e2d\u6587"
 say nihongo "\u65e5\u672c\u8a9e"
-say russkij "\u0420\u0443\u0441\u0441\u043a\u0438\u0439"
+
+set ::langoptions {
+	english francais deutsch catala espanol portugues brasileiro
+	italiano euskara bokmal dansk nederlands turkce polski russkij chinese nihongo
+	
+}
