998a35ae8afe07a1a8ced031b8527045092a712e
sevyves <sevyves@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2009-10-19 16:56:56 +0000

    fixed comments
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@12616 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- externals/pdp_opencv/pdp_opencv_of_lk.cc
+++ externals/pdp_opencv/pdp_opencv_of_lk.cc
@@ -16,7 +16,7 @@
  *   along with this program; if not, write to the Free Software
  *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
- *   Horn and Schunck optical flow algorithm
+ *   Lucas and Kanade optical flow algorithm
  *
  */
 
