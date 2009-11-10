4b381faf4a3bbd9c0714e419339850a037ef88e6
eighthave <eighthave@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2009-11-10 00:31:06 +0000

    updated for 0.43-pre
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@12732 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- externals/corelibs/generate.sh
+++ externals/corelibs/generate.sh
@@ -1,23 +1,25 @@
 #!/bin/sh
 
+echo this script appends, so first delete all files you want to update!
+
 # put these at the top of the file
 touch lib_d_fft.c
 echo '#include "../../pd/src/d_fftroutine.c"' >> lib_d_fft.c
-echo '#include "../../pd/src/d_mayer_fft.c"' >> lib_d_fft.c
+echo '#include "../../pd/src/d_fft_mayer.c"' >> lib_d_fft.c
 
 for file in ../../pd/src/[dx]_*.c; do 
 	 newfile=`echo $file | sed 's|.*/src/\([dx]_\)|lib_\1|'`
 	 touch $newfile
-	 echo -n '#include "' >> $newfile
-	 echo -n $file >> $newfile
-	 echo '"' >> $newfile
-	 echo "void "`echo $newfile|sed 's|\(.*\)\.c|\1|'`"_setup(void)" >> $newfile 
-	 echo "{" >> $newfile
-	 echo $file | sed 's|.*src/\(.*\)\.c|    \1_setup();|' >> $newfile
-	 echo "}" >> $newfile
+	 /bin/echo -n '#include "' >> $newfile
+	 /bin/echo -n $file >> $newfile
+	 /bin/echo '"' >> $newfile
+	 /bin/echo "void "`echo $newfile|sed 's|\(.*\)\.c|\1|'`"_setup(void)" >> $newfile 
+	 /bin/echo "{" >> $newfile
+	 /bin/echo $file | sed 's|.*src/\(.*\)\.c|    \1_setup();|' >> $newfile
+	 /bin/echo "}" >> $newfile
 done
 
 # these files hold code for other classes, but no classes
-rm lib_d_fftroutine.c lib_d_mayer_fft.c lib_d_resample.c
-
+rm lib_d_fftroutine.c lib_d_fft_mayer.c lib_d_resample.c
+rm lib_d_fft_fftw.c lib_d_fft_fftsg.c lib_d_fftsg_h.c
 
