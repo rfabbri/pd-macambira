535c982808d9efbe87afb05b8dce88d7b456cb58
mrpeach <mrpeach@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2010-05-04 16:07:41 +0000

    SO_REUSEADDR was not being set on linux.
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@13493 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- externals/mrpeach/net/tcpserver.c
+++ externals/mrpeach/net/tcpserver.c
@@ -913,7 +913,8 @@ static void *tcpserver_new(t_floatarg fportno)
     int                 i;
     struct sockaddr_in  server;
     int                 sockfd, portno = fportno;
-    char                optval = 1;
+    int                 optVal = 1;
+    int                 optLen = sizeof(int);
 
     /* create a socket */
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -928,7 +929,12 @@ static void *tcpserver_new(t_floatarg fportno)
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = INADDR_ANY;
     /* enable reuse of local address */
-    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) < 0)
+#ifdef _WIN32
+    if (setsockopt(sockfd, SOL_SOCKET, SO_SNDBUF, (char*)&optVal, optLen) == SOCKET_ERROR)
+#else
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (char*)&optVal, optLen) == -1)
+#endif
+//    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) < 0)
         sys_sockerror("tcpserver: setsockopt SO_REUSEADDR");
 
     /* assign server port number */
