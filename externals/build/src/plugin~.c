348dc54cc0a8cf723a5f547608b78a6495864eaa
eighthave <eighthave@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2010-06-09 20:16:32 +0000

    enable build on Windows, merged from pd-extended/0.42
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@13636 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- externals/build/src/plugin~.c
+++ externals/build/src/plugin~.c
@@ -1,6 +1,4 @@
-#if defined(__gnu_linux__) || defined(__APPLE__)
 #define HAVE_LOCALE_H
 #include "../../plugin~/jload.c"
 #include "../../plugin~/jsearch.c"
 #include "../../plugin~/plugin~.c"
-#endif
