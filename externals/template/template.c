957f514338b92f2162518276dbe23c9506464bc5
eighthave <eighthave@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2010-08-23 22:27:01 +0000

    moved optional example to wiki docs: http://puredata.info/docs/developer/MakefileTemplate
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@13903 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- externals/template/template.c
+++
@@ -1,8 +0,0 @@
-/* glue code for compiling "template" as a library
- * this calls the setup function of each object
- */
-void mycobject_setup(void);
-
-void template_setup(void) {
-  mycobject_setup();
-}
