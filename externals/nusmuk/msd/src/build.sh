0414e8e447997634cd52343811c3ef4a66d5413f
nusmuk <nusmuk@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2010-09-09 10:21:04 +0000

    update build script to compile on linux
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@14020 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- externals/nusmuk/msd/src/build.sh
+++ externals/nusmuk/msd/src/build.sh
@@ -13,8 +13,20 @@ cd ../src
 
 # move objects
 UNAME=`uname -s`
-if [ $UNAME==Darwin ]; then
+if [ $UNAME = "Darwin" ]
+then
 	mv ../msd/pd-darwin/release-single/msd.pd_darwin ../msd/msd.pd_darwin
 	mv ../msd2D/pd-darwin/release-single/msd2D.pd_darwin ../msd/msd2D.pd_darwin
 	mv ../msd3D/pd-darwin/release-single/msd3D.pd_darwin ../msd/msd3D.pd_darwin
+fi
+
+if [ $UNAME = "Linux" ]
+then
+    cd ../msd
+    ../../../grill/flext/build.sh pd gcc install
+    cd ../msd2D
+    ../../../grill/flext/build.sh pd gcc install
+    cd ../msd3D
+    ../../../grill/flext/build.sh pd gcc install
+    cd ../src
 fi
