bfa7d53a24c3bb476af2f7ab3a1264d88c4a855c
eighthave <eighthave@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2010-12-12 21:18:29 +0000

    added comment about only working on Pd-extended
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@14592 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- scripts/guiplugins/simple_examples/meters_default_on-plugin.tcl
+++ scripts/guiplugins/simple_examples/meters_default_on-plugin.tcl
@@ -1,3 +1,6 @@
-# this makes the meters in the Pd window be on by default
+# This makes the meters in the Pd window be on by default the meters
+# have been removed from Pure Data/Vanilla, so this only works on
+# Pd-extended.
+
 set ::meters 1
 pdsend "pd meters 1"
