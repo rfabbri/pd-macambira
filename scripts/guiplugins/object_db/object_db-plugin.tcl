c369b565d665531619090e1afbf38acb68554344
eighthave <eighthave@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2010-12-12 21:34:34 +0000

    search path for object_tags.tcllist
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@14594 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- scripts/guiplugins/object_db/object_db-plugin.tcl
+++ scripts/guiplugins/object_db/object_db-plugin.tcl
@@ -38,11 +38,17 @@ array set object_tags {}
 # tag reverse mapping
 array set objects_with_tag {}
 
-# load object -> tags mapping from file
-set f [open [file join $::current_pathdir object_tags.tcllist]]
-set tmp_db [read $f]
-close $f
-unset f
+# load object -> tags mapping from file in Pd's path
+foreach pathdir [concat $::sys_searchpath $::sys_staticpath] {
+    set dir [file normalize $pathdir]
+    set testfile [file join $dir object_tags.tcllist]
+    if { [file isfile $testfile]} {
+        set f [open $testfile]
+        set tmp_db [read $f]
+        close $f
+        unset f        
+    }
+}
 
 foreach {library object tags} $tmp_db {
     # skip unwanted libraries
