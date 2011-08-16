204282d451b9ee57668c2d023ed00e79ce719fc3
eighthave <eighthave@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2011-08-16 17:34:31 +0000

    add arrowheads to cords to show direction of message flow
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@15221 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

---
+++ scripts/guiplugins/simple_examples/cordarrows-plugin.tcl
@@ -0,0 +1,16 @@
+# this plugin adds arrowheads to the ends of cords when in editmode in
+# order to show the direction that the messages are flowing
+
+proc add_arrows_to_cords {mytoplevel} {
+    if { ! [winfo exists $mytoplevel] } {return}
+    if {$mytoplevel eq ".pdwindow"} {return}
+    set tkcanvas [tkcanvas_name $mytoplevel]
+    if {$::editmode($mytoplevel) == 1} {
+        $tkcanvas itemconfigure cord -arrow last
+    } else {
+        $tkcanvas itemconfigure cord -arrow none
+    }
+}
+
+
+bind all <<EditMode>> {+add_arrows_to_cords %W}
