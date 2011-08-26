570944b5c839c0854ebb710c988f83500cbed5c2
eighthave <eighthave@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2011-08-26 20:55:10 +0000

    updated for new default msys location
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@15279 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- scripts/auto-build/extra-rsync.bat
+++ scripts/auto-build/extra-rsync.bat
@@ -1,18 +1,16 @@
 REM uses http://setacl.sourceforge.net/
 
-cd \msys\1.0\home\pd\
+cd \MinGW\msys\1.0\home\pd\
 
-REM echo y|cacls c:\msys\1.0\home\pd\auto-build /C /T /G pd:F everyone:R 
+REM echo y|cacls c:\MinGW\msys\1.0\home\pd\auto-build /C /T /G pd:F everyone:R 
 
-setacl -on c:\msys\1.0\home\pd\auto-build -ot file -actn ace -ace "n:pd;p:full,write_owner;i:so,sc;m:set" -ace "n:everyone;p:read;i:so,sc;m:set"
+setacl -on c:\MinGW\msys\1.0\home\pd\auto-build -ot file -actn ace -ace "n:pd;p:full,write_owner;i:so,sc;m:set" -ace "n:everyone;p:read;i:so,sc;m:set"
 
 REM Cygwin rsync seems to be unhappy with SVN's .svn file permissions, so
 REM ignore SVN files first to get all the 'meat'
-rsync -av --progress --whole-file --exclude='*inv\**' --cvs-exclude --timeout=60 rsync://128.238.56.50/distros/pd-extended/ /home/pd/auto-build/pd-extended/
-rsync -av --progress --whole-file --exclude='*inv\**' --cvs-exclude --timeout=60 rsync://128.238.56.50/distros/pd-devel/ /home/pd/auto-build/pd-devel/
+rsync -av --progress --whole-file --exclude='*inv\**' --cvs-exclude --timeout=60 rsync://128.238.56.50/distros/pd-extended/ /cygdrive/c/MinGW/msys/1.0/home/pd/auto-build/pd-extended/
 
 sleep 60
 
 REM now get the SVN changes, this might fail a lot, especially on '.svn/entries'
-rsync -av --progress --whole-file --delete-before --exclude='*inv\**' --timeout=60 rsync://128.238.56.50/distros/pd-extended/ /home/pd/auto-build/pd-extended/
-rsync -av --progress --whole-file --delete-before --exclude='*inv\**' --timeout=60 rsync://128.238.56.50/distros/pd-devel/ /home/pd/auto-build/pd-devel/
+rsync -av --progress --whole-file --delete-before --exclude='*inv\**' --timeout=60 rsync://128.238.56.50/distros/pd-extended/ /cygdrive/c/MinGW/msys/1.0/home/pd/auto-build/pd-extended/
--- scripts/auto-build/run-automated-build.bat
+++ scripts/auto-build/run-automated-build.bat
@@ -1,4 +1,4 @@
 @echo off
 
-C:\msys\1.0\bin\sh.exe --login -i -c /home/pd/auto-build/pd-extended/scripts/auto-build/run-automated-builder
+C:\MinGW\msys\1.0\bin\sh.exe --login -i -c /home/pd/auto-build/pd-extended/scripts/auto-build/run-automated-builder
 
