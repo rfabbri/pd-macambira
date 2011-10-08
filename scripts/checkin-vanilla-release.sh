8a47b943880aa6e5f3c2822484f4264eedc6f0a3
eighthave <eighthave@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2011-10-08 04:25:09 +0000

    script to check a vanilla release tarball into the pure-data SVN
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@15543 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

---
+++ scripts/checkin-vanilla-release.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# this script is for checking in a Pd release tarball into the pure-data SVN
+
+cd ~/code/pure-data/trunk/pd
+rm -rf bin/*
+rm -rf src/autom4te.cache/
+find ~/code/pure-data/trunk/pd -type f | grep -v svn | xargs rm
+tar --strip-components 1 -xzf ~/Downloads/pd-0.43-0.src.tar.gz
+rm configure src/configure src/makefile.dependencies
+svn rm `svn st| sed -n 's|^!\(.*\)|\1|p'`
+svn add `svn st| sed -n 's|^?\(.*\)|\1|p'`
