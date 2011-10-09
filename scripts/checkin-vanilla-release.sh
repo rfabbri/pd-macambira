d4ca170b467eddbf4d434b9ce40f7f22b7f37a7c
eighthave <eighthave@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2011-10-09 16:42:31 +0000

    updated for pd-0.43-1test4.src.tar.gz
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@15559 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- scripts/checkin-vanilla-release.sh
+++ scripts/checkin-vanilla-release.sh
@@ -6,7 +6,7 @@ cd ~/code/pure-data/trunk/pd
 rm -rf bin/*
 rm -rf src/autom4te.cache/
 find ~/code/pure-data/trunk/pd -type f | grep -v svn | xargs rm
-tar --strip-components 1 -xzf ~/Downloads/pd-0.43-0.src.tar.gz
-rm configure src/configure src/makefile.dependencies
+tar --strip-components 1 -xzf ~/Downloads/pd-0.43-1test4.src.tar.gz
+rm -f configure src/configure src/makefile.dependencies
 svn rm `svn st| sed -n 's|^!\(.*\)|\1|p'`
 svn add `svn st| sed -n 's|^?\(.*\)|\1|p'`
