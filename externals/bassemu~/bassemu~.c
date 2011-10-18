6bea6edca6401b807e53468e5a9c6cc946e7e616
eighthave <eighthave@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2011-10-18 18:20:01 +0000

    use verbose() for version message
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@15615 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- externals/bassemu~/bassemu~.c
+++ externals/bassemu~/bassemu~.c
@@ -502,9 +502,9 @@ void bassemu_tilde_setup(void)
 	class_addmethod(bassemu_class, (t_method)bassemu_pw,	gensym("pw"),		A_DEFFLOAT, 0);
 	class_addmethod(bassemu_class, (t_method)bassemu_pitch,	gensym("pitch"),	A_DEFFLOAT, 0);
 
-	post("bassemu~: transistor bass emulation");
-	post("bassemu~: version %i.%i",VER_MAJ, VER_MIN);
-	post("bassemu~: (c) 2006 Ch. Klippel - ck@mamalala.de");
-	post("bassemu~: this is gpl'ed software, see README for details\n");
+	verbose(0, "bassemu~: transistor bass emulation");
+	verbose(0, "bassemu~: version %i.%i",VER_MAJ, VER_MIN);
+	verbose(0, "bassemu~: (c) 2006 Ch. Klippel - ck@mamalala.de");
+	verbose(0, "bassemu~: this is gpl'ed software, see README for details\n");
 }
 
