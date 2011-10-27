cf79af0374055d6f82862421ecd147e7e3320161
eighthave <eighthave@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2011-10-27 16:24:39 +0000

    actually makes symlinks now
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@15666 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- scripts/make-objectname-alias-symlinks.sh
+++ scripts/make-objectname-alias-symlinks.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 
-pd_app="$1"
-pd_app_guts="$pd_app/Contents/Resources/extra"
+pd_base="$1"
+pd_base_extra="$pd_base/extra"
 
-for file in `find "$pd_app_guts" -name '*0x*'`; do 
+for file in `find "$pd_base_extra" -name '*0x*'`; do 
     cd `dirname $file`
-    echo `dirname $file`
-    /sw/bin/echo -e `basename $file`
-    basename $file \
+    dir=`dirname $file`
+    if [ x$dir != x$olddir ]; then
+ 	echo $dir
+    fi
+    filename=`basename $file`
+    linkname=`basename $file \
         | sed 's|0x20| |g' \
-        | sed 's|0x21|!|g' \
-        | sed 's|0x22|"|g' \
-        | sed 's|0x23|#|g' \
-        | sed 's|0x24|\$|g' \
+        | sed 's|0x21|\\!|g' \
+        | sed 's|0x22|\\"|g' \
+        | sed 's|0x23|\\#|g' \
+        | sed 's|0x24|\\$|g' \
         | sed 's|0x25|%|g' \
         | sed 's|0x26|\&|g' \
         | sed 's|0x28|(|g' \
@@ -28,10 +31,15 @@ for file in `find "$pd_app_guts" -name '*0x*'`; do
         | sed 's|0x3c|<|g' \
         | sed 's|0x3d|=|g' \
         | sed 's|0x3e|>|g' \
+        | sed 's|0x3f|?|g' \
+        | sed 's|0x40|@|g' \
         | sed 's|0x5e|^|g' \
         | sed 's/0x7c/|/g' \
-        | sed 's|0x7e|~|g'
-    echo ""
+        | sed 's|0x7e|~|g'`
+#        | sed "s|\(.*\)|'\1'|g"`
+#    echo "linkname == $linkname =="
+    ln -s $filename $linkname
+    olddir=$dir
     cd ..
 done
 
