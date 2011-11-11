91d84ac920c58163e64f604bcb87c025a3a103ee
eighthave <eighthave@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2011-11-11 23:33:31 +0000

    ported to 'variable ' technique and use proc instead of proc+
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@15726 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- externals/tclfile/exists.tcl
+++ externals/tclfile/exists.tcl
@@ -1,10 +1,13 @@
 package require Tclpd 0.2.3
 package require TclpdLib 0.19
 
-proc+ exists::constructor {self args} {
-    set @current_canvas [canvas_getcurrent]
+proc exists::constructor {self args} {
+    if {![namespace exists $self]} {
+        namespace eval $self {}
+    }
+    variable ${self}::current_canvas [canvas_getcurrent]
     # set to blank so the var always exists
-    set @filename {}
+    variable ${self}::filename {}
 
     # add second inlet (first created by default)
     pd::add_inlet $self list
@@ -13,24 +16,26 @@ proc+ exists::constructor {self args} {
     pd::add_outlet $self list
 }
 
-proc+ exists::0_symbol {self args} {
+proc exists::0_symbol {self args} {
     # HOT inlet
-    set @filename [pd::arg 0 symbol]
+    variable ${self}::filename [pd::arg 0 symbol]
     exists::0_bang $self
 }
 
-proc+ exists::0_bang {self} {
-    if {[file pathtype $@filename] eq "absolute"} {
-        pd::outlet $self 0 float [file exists $@filename]
+proc exists::0_bang {self} {
+    variable ${self}::current_canvas
+    variable ${self}::filename
+    if {[file pathtype $filename] eq "absolute"} {
+        pd::outlet $self 0 float [file exists $filename]
     } else {
-        set dir [[canvas_getdir $@current_canvas] cget -s_name]
-        pd::outlet $self 0 float [file exists [file join $dir $@filename]]
+        set dir [[canvas_getdir $current_canvas] cget -s_name]
+        pd::outlet $self 0 float [file exists [file join $dir $filename]]
     }
 }
 
-proc+ exists::1_symbol {self args} {
+proc exists::1_symbol {self args} {
     # COLD inlet
-    set @filename [pd::arg 0 symbol]
+    variable ${self}::filename [pd::arg 0 symbol]
 }
 
 pd::class exists
--- externals/tclfile/mkdir.tcl
+++ externals/tclfile/mkdir.tcl
@@ -1,33 +1,38 @@
 package require Tclpd 0.2.3
 package require TclpdLib 0.19
 
-proc+ mkdir::constructor {self args} {
+proc mkdir::constructor {self args} {
+    if {![namespace exists $self]} {
+        namespace eval $self {}
+    }
+    variable ${self}::filename
     set @current_canvas [canvas_getcurrent]
     # set to blank so the var always mkdir
-    set @filename {}
+    set filename {}
 
     # add second inlet (first created by default)
     pd::add_inlet $self list
 }
 
-proc+ mkdir::0_symbol {self args} {
+proc mkdir::0_symbol {self args} {
     # HOT inlet
-    set @filename [pd::arg 0 symbol]
+    variable ${self}::filename [pd::arg 0 symbol]
     mkdir::0_bang $self
 }
 
-proc+ mkdir::0_bang {self} {
-    if {[file pathtype $@filename] eq "absolute"} {
-        file mkdir $@filename
+proc mkdir::0_bang {self} {
+    variable ${self}::filename
+    if {[file pathtype $filename] eq "absolute"} {
+        file mkdir $filename
     } else {
-        set dir [[canvas_getdir $@current_canvas] cget -s_name]
-        file mkdir [file join $dir $@filename]
+        set dir [[canvas_getdir $current_canvas] cget -s_name]
+        file mkdir [file join $dir $filename]
     }
 }
 
 proc+ mkdir::1_symbol {self args} {
     # COLD inlet
-    set @filename [pd::arg 0 symbol]
+    variable ${self}::filename [pd::arg 0 symbol]
 }
 
 pd::class mkdir
