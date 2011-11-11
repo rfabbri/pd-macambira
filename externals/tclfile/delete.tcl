4b3b7273167274611ee80bfe7e82f3d60f5ce033
eighthave <eighthave@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2011-11-11 23:43:03 +0000

    create [delete] object, but no handling of -force or lists of files yet
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@15728 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

---
+++ externals/tclfile/delete.tcl
@@ -0,0 +1,38 @@
+package require Tclpd 0.2.3
+package require TclpdLib 0.19
+
+proc delete::constructor {self args} {
+    if {![namespace exists $self]} {
+        namespace eval $self {}
+    }
+    # set to blank so the var always delete
+    variable ${self}::filename {}
+    variable ${self}::current_canvas [canvas_getcurrent]
+
+    # add second inlet (first created by default)
+    pd::add_inlet $self list
+}
+
+proc delete::0_symbol {self args} {
+    # HOT inlet
+    variable ${self}::filename [pd::arg 0 symbol]
+    delete::0_bang $self
+}
+
+proc delete::0_bang {self} {
+    variable ${self}::current_canvas
+    variable ${self}::filename
+    if {[file pathtype $filename] eq "absolute"} {
+        file delete -- $filename
+    } else {
+        set dir [[canvas_getdir $current_canvas] cget -s_name]
+        file delete -- [file join $dir $filename]
+    }
+}
+
+proc+ delete::1_symbol {self args} {
+    # COLD inlet
+    variable ${self}::filename [pd::arg 0 symbol]
+}
+
+pd::class delete
