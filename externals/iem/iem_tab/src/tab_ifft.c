f5e5e8e776c3864b1858e1328d9518536548e685
tmusil <tmusil@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2011-11-16 12:17:51 +0000

    in setup replaced 2nd dst_re by dst_im method
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@15758 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- externals/iem/iem_tab/src/tab_ifft.c
+++ externals/iem/iem_tab/src/tab_ifft.c
@@ -401,7 +401,7 @@ void tab_ifft_setup(void)
   class_addmethod(tab_ifft_class, (t_method)tab_ifft_src_re, gensym("src1"), A_DEFSYMBOL, 0);
   class_addmethod(tab_ifft_class, (t_method)tab_ifft_src_im, gensym("src2"), A_DEFSYMBOL, 0);
   class_addmethod(tab_ifft_class, (t_method)tab_ifft_dst_re, gensym("dst_re"), A_DEFSYMBOL, 0);
-  class_addmethod(tab_ifft_class, (t_method)tab_ifft_dst_im, gensym("dst_re"), A_DEFSYMBOL, 0);
+  class_addmethod(tab_ifft_class, (t_method)tab_ifft_dst_im, gensym("dst_im"), A_DEFSYMBOL, 0);
   class_addmethod(tab_ifft_class, (t_method)tab_ifft_dst_re, gensym("dst1"), A_DEFSYMBOL, 0);
   class_addmethod(tab_ifft_class, (t_method)tab_ifft_dst_im, gensym("dst2"), A_DEFSYMBOL, 0);
 //  class_sethelpsymbol(tab_ifft_class, gensym("iemhelp2/tab_ifft-help"));
