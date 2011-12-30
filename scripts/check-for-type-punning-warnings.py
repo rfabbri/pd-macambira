df4b0313204192985747e49e319e8c7863329e61
eighthave <eighthave@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2011-12-30 20:32:07 +0000

    comment out section that sends log in an email
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@15858 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- scripts/check-for-type-punning-warnings.py
+++ scripts/check-for-type-punning-warnings.py
@@ -28,32 +28,32 @@ for line in logoutput:
 	lf.write(line)
 lf.close()
 
-# make the email report
-fromaddr = 'pd@pdlab.idmi.poly.edu'
-toaddr = 'hans@at.or.at'
-mailoutput = []
-mailoutput.append('From: ' + fromaddr + '\n')
-mailoutput.append('To: ' + toaddr + '\n')
-mailoutput.append('Subject: type-punning warnings ' + date + '\n\n\n')
-mailoutput.append('______________________________________________________________________\n\n')
-mailoutput.append('Complete log:\n')
-mailoutput.append('http://autobuild.puredata.info/auto-build/' + date + '/logs/'
-                  + outputfilename + '\n')
+# # make the email report
+# fromaddr = 'pd@pdlab.idmi.poly.edu'
+# toaddr = 'hans@at.or.at'
+# mailoutput = []
+# mailoutput.append('From: ' + fromaddr + '\n')
+# mailoutput.append('To: ' + toaddr + '\n')
+# mailoutput.append('Subject: type-punning warnings ' + date + '\n\n\n')
+# mailoutput.append('______________________________________________________________________\n\n')
+# mailoutput.append('Complete log:\n')
+# mailoutput.append('http://autobuild.puredata.info/auto-build/' + date + '/logs/'
+#                   + outputfilename + '\n')
 
 
-# upload the log file to the autobuild website
-rsyncfile = 'rsync://128.238.56.50/upload/' + date + '/logs/' + outputfilename
-cmd = ['rsync', '-axv', outputfile, rsyncfile]
-mailoutput.append(' '.join(cmd) + '\n')
-p = subprocess.Popen(cmd,
-					 shell=False,
-					 stdout=subprocess.PIPE,
-					 stderr=subprocess.PIPE)
-p.wait()
-mailoutput.append(''.join(p.stdout.readlines()))
-mailoutput.append(''.join(p.stderr.readlines()))
+# # upload the log file to the autobuild website
+# rsyncfile = 'rsync://128.238.56.50/upload/' + date + '/logs/' + outputfilename
+# cmd = ['rsync', '-axv', outputfile, rsyncfile]
+# mailoutput.append(' '.join(cmd) + '\n')
+# p = subprocess.Popen(cmd,
+# 					 shell=False,
+# 					 stdout=subprocess.PIPE,
+# 					 stderr=subprocess.PIPE)
+# p.wait()
+# mailoutput.append(''.join(p.stdout.readlines()))
+# mailoutput.append(''.join(p.stderr.readlines()))
 
-mailoutput.append('______________________________________________________________________\n\n')
-server = smtplib.SMTP('in1.smtp.messagingengine.com')
-server.sendmail(fromaddr, toaddr, ''.join(mailoutput + logoutput))
-server.quit()
+# mailoutput.append('______________________________________________________________________\n\n')
+# server = smtplib.SMTP('in1.smtp.messagingengine.com')
+# server.sendmail(fromaddr, toaddr, ''.join(mailoutput + logoutput))
+# server.quit()
