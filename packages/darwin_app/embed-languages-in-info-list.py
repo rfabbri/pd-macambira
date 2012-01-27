aa50f98d48e5aeb839c6f1fb73b03898fe0bba71
eighthave <eighthave@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2012-01-27 21:30:19 +0000

    embed the included languages into the Info.plist to enable per-app language selection
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@15894 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

---
+++ packages/darwin_app/embed-languages-in-info-list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import sys
+import re
+
+f = open(sys.argv[1], 'r')
+for line in f.readlines():
+    if re.match('^ALL_LINGUAS = (.*)', line):
+        for po in line.split('=')[1].strip().rstrip().split(' '):
+            s = po.split('_')
+            if len(s) == 2:
+                iso = s[0] + '_' + s[1].upper()
+            else:
+                iso = po
+            print '<string>' + iso + '</string>\\n\\t',
