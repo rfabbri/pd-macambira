0a849b9fd6e255ed0ca57480ff41b35e0deb670e
eighthave <eighthave@df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d>
2012-01-30 03:31:12 +0000

    post version message using verbose(0)
    git-svn-id: https://pure-data.svn.sourceforge.net/svnroot/pure-data/trunk@15915 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- externals/vbap/vbap.c
+++ externals/vbap/vbap.c
@@ -95,7 +95,7 @@ void vbap_setup(void)
     class_addmethod(vbap_class, (t_method)def_ls_read_directions, gensym("ls-directions"), A_GIMME, 0);	
     class_addmethod(vbap_class, (t_method)def_ls_read_triplets, gensym("ls-triplets"), A_GIMME, 0);
 
-	verbose(-1, VBAP_VERSION);
+	verbose(0, VBAP_VERSION);
 }
 #else /* MAX */
 void main(void)
